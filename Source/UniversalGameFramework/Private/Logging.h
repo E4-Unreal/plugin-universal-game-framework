c53222bce9a9962a91adf26b3f9310840f9782cc
이정훈 <eu4ng97@gmail.com>
2025-03-19 17:42:56 +0900

    Feat: UniversalGameFramework 모듈 전용 로그 매크로 파일 추가

---
+++ Source/UniversalGameFramework/Private/Logging.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "UniversalUtilities.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogUniversalGameFramework, Log, All)
+
+#undef CATEGORY
+#define CATEGORY LogUniversalGameFramework
--- Source/UniversalGameFramework/UniversalGameFramework.Build.cs
+++ Source/UniversalGameFramework/UniversalGameFramework.Build.cs
@@ -10,6 +10,7 @@ public class UniversalGameFramework : ModuleRules
             new string[]
             {
                 "Core",
+                "UniversalUtilities"
             }
         );
 
