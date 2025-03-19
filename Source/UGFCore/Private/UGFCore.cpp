a3c4452a474f2cdd1dc5e80d953f25605ca933d5
이정훈 <eu4ng97@gmail.com>
2025-03-19 17:37:21 +0900

    Chore: UGFCore 모듈 생성

---
+++ Source/UGFCore/Private/UGFCore.cpp
@@ -0,0 +1,17 @@
+﻿#include "UGFCore.h"
+
+#define LOCTEXT_NAMESPACE "FUGFCoreModule"
+
+void FUGFCoreModule::StartupModule()
+{
+    
+}
+
+void FUGFCoreModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUGFCoreModule, UGFCore)
