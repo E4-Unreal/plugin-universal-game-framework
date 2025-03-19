1ea103e940d424973be19e7ff534d66283f4dd8f
이정훈 <eu4ng97@gmail.com>
2025-03-19 17:41:06 +0900

    Chore: UniversalGameFramework 모듈 생성

---
+++ Source/UniversalGameFramework/Private/UniversalGameFramework.cpp
@@ -0,0 +1,17 @@
+﻿#include "UniversalGameFramework.h"
+
+#define LOCTEXT_NAMESPACE "FUniversalGameFrameworkModule"
+
+void FUniversalGameFrameworkModule::StartupModule()
+{
+    
+}
+
+void FUniversalGameFrameworkModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUniversalGameFrameworkModule, UniversalGameFramework)
