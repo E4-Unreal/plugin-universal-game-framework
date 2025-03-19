76f80ba42162a425cd73ab9e60d600c28d60791a
이정훈 <eu4ng97@gmail.com>
2025-03-19 19:23:42 +0900

    Chore: UGFItemSystem 모듈 생성

---
+++ Source/UGFItemSystem/Private/UGFItemSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "UGFItemSystem.h"
+
+#define LOCTEXT_NAMESPACE "FUGFItemSystemModule"
+
+void FUGFItemSystemModule::StartupModule()
+{
+    
+}
+
+void FUGFItemSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUGFItemSystemModule, UGFItemSystem)
---
+++ Source/UGFItemSystem/UGFItemSystem.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class UGFItemSystem : ModuleRules
+{
+    public UGFItemSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
