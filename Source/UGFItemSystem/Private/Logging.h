9056bc658d624e4deab209c331e91775bd9afa4d
이정훈 <eu4ng97@gmail.com>
2025-03-19 19:24:40 +0900

    Feat: UGFItemSystem 로그 매크로 파일 추가 및 UGFCore 종속성 추가

---
+++ Source/UGFItemSystem/Private/Logging.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "UniversalUtilities.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogUGFItemSystem, Log, All)
+
+#undef CATEGORY
+#define CATEGORY LogUGFItemSystem
--- Source/UGFItemSystem/UGFItemSystem.Build.cs
+++ Source/UGFItemSystem/UGFItemSystem.Build.cs
@@ -10,6 +10,8 @@ public class UGFItemSystem : ModuleRules
             new string[]
             {
                 "Core",
+                "UniversalUtilities",
+                "UGFCore"
             }
         );
 
