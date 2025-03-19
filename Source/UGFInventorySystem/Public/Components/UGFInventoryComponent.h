380af10ccab4a6633916c2d4b3fc0f1f79ccbbee
이정훈 <eu4ng97@gmail.com>
2025-03-19 20:22:41 +0900

    Feat: UGFInventoryComponent 컴포넌트 클래스 작성

---
+++ Source/UGFInventorySystem/Public/Components/UGFInventoryComponent.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "UGFInventoryComponent.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class UGFINVENTORYSYSTEM_API UUGFInventoryComponent : public UActorComponent
+{
+    GENERATED_BODY()
+};
