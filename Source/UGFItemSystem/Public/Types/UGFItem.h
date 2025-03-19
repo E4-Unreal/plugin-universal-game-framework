2dbc323d7ea24cdc1b91c5d2f3fde2c3638837a7
이정훈 <eu4ng97@gmail.com>
2025-03-19 20:51:50 +0900

    Feat: UGFItem 구조체 작성

---
+++ Source/UGFItemSystem/Public/Types/UGFItem.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "UGFItem.generated.h"
+
+class UUGFItemDefinition;
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
+struct UGFITEMSYSTEM_API FUGFItem
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UUGFItemDefinition> ItemDefinition;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 1))
+    int32 Amount = 1;
+};
