6480bf8795784eb6b7cd0dd129864653dcf1a44c
이정훈 <eu4ng97@gmail.com>
2025-03-19 18:08:10 +0900

    Chore: UGFInventorySystem 모듈 생성

---
+++ Source/UGFInventorySystem/Private/UGFInventorySystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "UGFInventorySystem.h"
+
+#define LOCTEXT_NAMESPACE "FUGFInventorySystemModule"
+
+void FUGFInventorySystemModule::StartupModule()
+{
+    
+}
+
+void FUGFInventorySystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUGFInventorySystemModule, UGFInventorySystem)
