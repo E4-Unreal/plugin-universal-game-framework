134d8a2adf9e605a97d6fa9610fc0741fc4123c0
이정훈 <eu4ng97@gmail.com>
2025-03-23 03:10:21 +0900

    Feat: UGFInventorySlot 구조체 작성

---
+++ Source/UGFInventorySystem/Public/Types/UGFInventorySlot.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "UGFInventorySlot.generated.h"
+
+class UUGFInventoryItemConfig;
+class UUGFItemDefinition;
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
+struct UGFINVENTORYSYSTEM_API FUGFInventorySlot
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 0))
+    int32 Index;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UUGFItemDefinition> ItemDefinition;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 1))
+    int32 Quantity = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UUGFInventoryItemConfig> InventoryItemConfig;
+};
