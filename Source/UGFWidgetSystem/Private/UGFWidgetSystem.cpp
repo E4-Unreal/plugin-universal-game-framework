b89ec7b89a96560f359698a5abb4984c10f11e9a
이정훈 <eu4ng97@gmail.com>
2025-03-27 21:01:11 +0900

    Chore: UGFWidgetSystem 모듈 생성

---
+++ Source/UGFWidgetSystem/Private/UGFWidgetSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "UGFWidgetSystem.h"
+
+#define LOCTEXT_NAMESPACE "FUGFWidgetSystemModule"
+
+void FUGFWidgetSystemModule::StartupModule()
+{
+    
+}
+
+void FUGFWidgetSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUGFWidgetSystemModule, UGFWidgetSystem)
---
+++ Source/UGFWidgetSystem/UGFWidgetSystem.Build.cs
@@ -0,0 +1,28 @@
+﻿using UnrealBuildTool;
+
+public class UGFWidgetSystem : ModuleRules
+{
+    public UGFWidgetSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities",
+                "UGFCore"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
