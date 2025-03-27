ba8a9ed8dac375d537128df2c8c18ff621ae7039
이정훈 <eu4ng97@gmail.com>
2025-03-27 18:45:02 +0900

    Feat: UGFInputConfig 클래스 작성

---
+++ Source/UGFInputSystem/Private/Input/UGFInputConfig.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/UGFInputConfig.h"
+
+void UUGFInputConfig::BindEnhancedInput(UEnhancedInputComponent* EnhancedInputComponent)
+{
+    if (EnhancedInputComponent == nullptr) return;
+
+    OnBindEnhancedInput(EnhancedInputComponent);
+}
---
+++ Source/UGFInputSystem/Public/Input/UGFInputConfig.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "UGFInputConfig.generated.h"
+
+/**
+ * 입력 바인딩 전용 데이터 에셋
+ */
+UCLASS(Abstract)
+class UGFINPUTSYSTEM_API UUGFInputConfig : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    void BindEnhancedInput(UEnhancedInputComponent* EnhancedInputComponent);
+
+protected:
+    virtual void OnBindEnhancedInput(UEnhancedInputComponent* EnhancedInputComponent) { }
+};
