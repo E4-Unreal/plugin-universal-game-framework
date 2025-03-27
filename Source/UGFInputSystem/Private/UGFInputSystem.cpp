954b76c058533c8fba69193e6684cce82501eeed
이정훈 <eu4ng97@gmail.com>
2025-03-27 16:10:05 +0900

    Chore: UGFInputSystem 모듈 생성

---
+++ Source/UGFInputSystem/Private/UGFInputSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "UGFInputSystem.h"
+
+#define LOCTEXT_NAMESPACE "FUGFInputSystemModule"
+
+void FUGFInputSystemModule::StartupModule()
+{
+    
+}
+
+void FUGFInputSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUGFInputSystemModule, UGFInputSystem)
---
+++ Source/UGFInputSystem/UGFInputSystem.Build.cs
@@ -0,0 +1,28 @@
+﻿using UnrealBuildTool;
+
+public class UGFInputSystem : ModuleRules
+{
+    public UGFInputSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities",
+                "UGFCore"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
