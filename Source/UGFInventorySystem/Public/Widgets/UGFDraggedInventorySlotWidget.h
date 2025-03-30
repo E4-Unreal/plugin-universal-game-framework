9b08528591658ca946bfeb91905a86a54c311d04
이정훈 <eu4ng97@gmail.com>
2025-03-31 00:07:35 +0900

    Feat: UGFDraggedInventorySlotWidget 클래스 작성

---
+++ Source/UGFInventorySystem/Public/Widgets/UGFDraggedInventorySlotWidget.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UGFDraggedInventorySlotWidget.generated.h"
+
+class UImage;
+
+/**
+ *
+ */
+UCLASS()
+class UGFINVENTORYSYSTEM_API UUGFDraggedInventorySlotWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+private:
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UImage> ThumbnailImage;
+
+public:
+    UFUNCTION(BlueprintCallable)
+    virtual void SetThumbnailImage(TSoftObjectPtr<UTexture2D> ThumbnailTexture);
+};
