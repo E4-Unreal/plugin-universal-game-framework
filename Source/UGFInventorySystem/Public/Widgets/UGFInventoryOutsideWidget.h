d97763f91ad69b16eb8d553521a349102d808aa1
이정훈 <eu4ng97@gmail.com>
2025-04-05 01:17:16 +0900

    Feat: UGFInventoryOutsideWidget 클래스 작성

---
+++ Source/UGFInventorySystem/Public/Widgets/UGFInventoryOutsideWidget.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UGFInventoryOutsideWidget.generated.h"
+
+class UUGFInventoryComponent;
+
+/**
+ *
+ */
+UCLASS()
+class UGFINVENTORYSYSTEM_API UUGFInventoryOutsideWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+    UPROPERTY(Transient)
+    TObjectPtr<UUGFInventoryComponent> InventoryComponent;
+
+protected:
+    virtual void NativeOnInitialized() override;
+    virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+};
