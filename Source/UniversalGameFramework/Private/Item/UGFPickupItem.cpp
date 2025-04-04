a13c3deb05e3f5cdafaf044dcfb9f499ecd6683d
이정훈 <eu4ng97@gmail.com>
2025-04-04 20:24:51 +0900

    Feat: UGFPickupItem 클래스 작성

---
+++ Source/UniversalGameFramework/Private/Item/UGFPickupItem.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/UGFPickupItem.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/UGFInventoryComponent.h"
+
+AUGFPickupItem::AUGFPickupItem(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    OverlapSphere = CreateDefaultSubobject<USphereComponent>(FName("OverlapSphere"));
+    SetRootComponent(OverlapSphere);
+    DisplayMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("DisplayMesh"));
+    DisplayMesh->SetupAttachment(RootComponent);
+}
+
+void AUGFPickupItem::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetOverlapSphere()->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapSphereBeginOverlap);
+}
+
+void AUGFPickupItem::OnOverlapSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (bAutoPickup)
+    {
+        if (auto OtherPawn = Cast<APawn>(OtherActor))
+        {
+            if (auto InventoryComponent = OtherPawn->GetComponentByClass<UUGFInventoryComponent>())
+            {
+                for (auto Item : Items)
+                {
+                    int32 Overflow;
+                    InventoryComponent->AddItem(Item, Overflow);
+                }
+
+                Destroy();
+            }
+        }
+    }
+}
---
+++ Source/UniversalGameFramework/Public/Item/UGFPickupItem.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Types/UGFItem.h"
+#include "UGFPickupItem.generated.h"
+
+class USphereComponent;
+
+UCLASS(Abstract)
+class UNIVERSALGAMEFRAMEWORK_API AUGFPickupItem : public AActor
+{
+    GENERATED_BODY()
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<USphereComponent> OverlapSphere;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UStaticMeshComponent> DisplayMesh;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TArray<FUGFItem> Items;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    bool bAutoPickup = true;
+
+public:
+    AUGFPickupItem(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE USphereComponent* GetOverlapSphere() const { return OverlapSphere; }
+
+protected:
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    virtual void OnOverlapSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
