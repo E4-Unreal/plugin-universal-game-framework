0d86569625552e57bcf638293ff60d0e8f54f676
이정훈 <eu4ng97@gmail.com>
2025-04-11 22:32:01 +0900

    Feat: ItemSystemFunctionLibrary 클래스 작성

---
+++ Source/ItemSystem/Private/ItemSystemFunctionLibrary.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemSystemFunctionLibrary.h"
+
+#include "Data/ItemConfig.h"
+#include "Data/ItemDefinition.h"
+
+const UItemConfig* UItemSystemFunctionLibrary::FindItemConfigByClass(UItemDefinition* ItemDefinition,
+                                                                     TSubclassOf<UItemConfig> ItemConfigClass)
+{
+    if (ItemDefinition != nullptr && ItemConfigClass != nullptr)
+    {
+        return ItemDefinition->GetItemConfigByClass(ItemConfigClass);
+    }
+    return nullptr;
+}
---
+++ Source/ItemSystem/Public/ItemSystemFunctionLibrary.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ItemSystemFunctionLibrary.generated.h"
+
+class UItemConfig;
+class UItemDefinition;
+
+/**
+ *
+ */
+UCLASS()
+class ITEMSYSTEM_API UItemSystemFunctionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+    UFUNCTION(BlueprintCallable, meta = (DeterminesOutputType = ItemConfigClass))
+    static const UItemConfig* FindItemConfigByClass(UItemDefinition* ItemDefinition, TSubclassOf<UItemConfig> ItemConfigClass);
+};
