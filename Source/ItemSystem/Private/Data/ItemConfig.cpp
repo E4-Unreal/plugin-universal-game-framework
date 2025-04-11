f750d0347c99692d1eefdeb61baae214b3bc9cd5
이정훈 <eu4ng97@gmail.com>
2025-04-11 22:22:06 +0900

    Feat: ItemConfig 클래스 작성

---
+++ Source/ItemSystem/Private/Data/ItemConfig.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/ItemConfig.h"
+
+bool UItemConfig::Update(UItemDefinition* ItemDefinition, FItemDataTableRow* Row)
+{
+    if (ItemDefinition == nullptr || Row == nullptr)
+    {
+        bValid = false;
+        return false;
+    }
+
+    bValid = true;
+
+    return OnUpdate(ItemDefinition, Row);
+}
+
+bool UItemConfig::OnUpdate(UItemDefinition* ItemDefinition, FItemDataTableRow* Row)
+{
+    return true;
+}
---
+++ Source/ItemSystem/Public/Data/ItemConfig.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ItemConfig.generated.h"
+
+class UItemDefinition;
+struct FItemDataTableRow;
+
+/**
+ *
+ */
+UCLASS(Abstract, DefaultToInstanced, EditInlineNew, CollapseCategories)
+class ITEMSYSTEM_API UItemConfig : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(VisibleDefaultsOnly, Category = "State")
+    bool bValid = true;
+
+public:
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE bool IsValid() const { return bValid; }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE bool IsNotValid() const { return !IsValid(); }
+
+    bool Update(UItemDefinition* ItemDefinition, FItemDataTableRow* Row);
+
+protected:
+    virtual bool OnUpdate(UItemDefinition* ItemDefinition, FItemDataTableRow* Row);
+};
