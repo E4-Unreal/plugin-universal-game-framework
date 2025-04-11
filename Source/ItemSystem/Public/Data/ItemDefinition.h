01dd8a91cc761e768d18ae588e98a4d37beff749
이정훈 <eu4ng97@gmail.com>
2025-04-11 18:38:15 +0900

    Feat: ItemDefinition 클래스 작성

---
+++ Source/ItemSystem/Public/Data/ItemDefinition.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ItemDefinition.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ITEMSYSTEM_API UItemDefinition : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Config", meta = (ClampMin = 0))
+    int32 ID;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Config")
+    FText DisplayText;
+
+public:
+    virtual void Update(int32 NewID, const FText& NewDisplayText);
+};
