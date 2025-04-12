cffad7ecb84db411082d5487dbb8b602f047913d
이정훈 <eu4ng97@gmail.com>
2025-04-12 12:29:07 +0900

    Feat: ItemActorBase 클래스 작성

---
+++ Source/ItemSystem/Private/Actors/ItemActorBase.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/ItemActorBase.h"
+
+FName AItemActorBase::DisplayMeshName(TEXT("DisplayMesh"));
+
+AItemActorBase::AItemActorBase(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    /* DisplayMesh */
+    DisplayMesh = CreateDefaultSubobject<UStaticMeshComponent>(DisplayMeshName);
+}
---
+++ Source/ItemSystem/Public/Actors/ItemActorBase.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Types/ItemData.h"
+#include "ItemActorBase.generated.h"
+
+UCLASS()
+class ITEMSYSTEM_API AItemActorBase : public AActor
+{
+    GENERATED_BODY()
+
+protected:
+    static FName DisplayMeshName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UStaticMeshComponent> DisplayMesh;
+
+protected:
+    UPROPERTY(EditAnywhere, Category = "Config")
+    FItemData ItemData;
+
+public:
+    AItemActorBase(const FObjectInitializer& ObjectInitializer);
+
+    FORCEINLINE UStaticMeshComponent* GetDisplayMesh() const { return DisplayMesh; }
+};
