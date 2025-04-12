7a21f2895f068d5b1ef321a59255e027ea637ae2
이정훈 <eu4ng97@gmail.com>
2025-04-12 11:43:00 +0900

    Feat: ItemSystemSettings 클래스 작성

---
+++ Source/ItemSystem/Private/ItemSystemSettings.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemSystemSettings.h"
+
+UItemSystemSettings::UItemSystemSettings()
+{
+    CategoryName = "Plugins";
+    SectionName = "ItemSystem";
+}
+
+UItemDatabase* UItemSystemSettings::GetItemDatabase() const
+{
+    return ItemDatabase.LoadSynchronous();
+}
