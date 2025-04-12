b96f7c23d970cd0cbd7cfb1754243b12e09457f7
이정훈 <eu4ng97@gmail.com>
2025-04-12 11:45:32 +0900

    Feat: ItemSubsystem 클래스 작성

---
+++ Source/ItemSystem/Public/Subsystems/ItemSubsystem.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ItemSubsystem.generated.h"
+
+class UItemDefinition;
+
+/**
+ *
+ */
+UCLASS()
+class ITEMSYSTEM_API UItemSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable)
+    UItemDefinition* GetItemDefinitionByID(int32 ID);
+};
