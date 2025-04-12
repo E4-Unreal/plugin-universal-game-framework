9ee31218c538d599ee4205c6f4eff1d495655ed6
이정훈 <eu4ng97@gmail.com>
2025-04-12 12:57:10 +0900

    Feat: DefaultItemDataTableRow 구조체 작성

---
+++ Source/ItemSystem/Public/Types/DefaultItemDataTableRow.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemDataTableRow.h"
+#include "Interfaces/DisplayItemDataInterface.h"
+#include "UObject/Object.h"
+#include "DefaultItemDataTableRow.generated.h"
+
+USTRUCT()
+struct FDefaultItemDataTableRow : public FItemDataTableRow, public IDisplayItemDataInterface
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TSoftObjectPtr<UStaticMesh> StaticMesh;
+
+    /* IDisplayItemDataInterface */
+
+    virtual UStaticMesh* GetStaticMesh() const override { return StaticMesh.LoadSynchronous(); }
+};
