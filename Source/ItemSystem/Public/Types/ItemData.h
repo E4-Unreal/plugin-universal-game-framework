f50be27b09bcbdac7d1a5a093d54892c1973bafa
이정훈 <eu4ng97@gmail.com>
2025-04-12 11:30:57 +0900

    Feat: ItemData 구조체 작성

---
+++ Source/ItemSystem/Public/Types/ItemData.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ItemData.generated.h"
+
+class UItemDefinition;
+
+USTRUCT(Atomic, BlueprintType)
+struct ITEMSYSTEM_API FItemData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 ID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UItemDefinition> ItemDefinition;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Quantity;
+};
