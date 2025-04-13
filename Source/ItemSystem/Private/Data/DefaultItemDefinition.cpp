04203549e1c43000b2572b9badfe249ffcecaa06
이정훈 <eu4ng97@gmail.com>
2025-04-14 04:13:15 +0900

    Feat: DefaultItemDefinition 클래스 작성

---
+++ Source/ItemSystem/Private/Data/DefaultItemDefinition.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/DefaultItemDefinition.h"
+
+#include "Data/ItemConfig_Actor.h"
+#include "Types/DefaultItemDataTableRow.h"
+
+void UDefaultItemDefinition::UpdateItemConfigs(FTableRowBase* TableRow)
+{
+    Super::UpdateItemConfigs(TableRow);
+
+    UpdateActorItemConfig(TableRow);
+}
+
+void UDefaultItemDefinition::UpdateActorItemConfig(FTableRowBase* TableRow)
+{
+    if (FDefaultItemDataTableRow* ItemDataTableRow = static_cast<FDefaultItemDataTableRow*>(TableRow))
+    {
+        FItemConfigData_Actor NewData;
+        NewData.StaticMesh = ItemDataTableRow->StaticMesh;
+
+        UItemConfig_Actor* ItemConfig = GetOrCreateItemConfig<UItemConfig_Actor>();
+        if (ItemConfig) ItemConfig->Update(&NewData);
+    }
+}
---
+++ Source/ItemSystem/Public/Data/DefaultItemDefinition.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemDefinition.h"
+#include "DefaultItemDefinition.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ITEMSYSTEM_API UDefaultItemDefinition : public UItemDefinition
+{
+    GENERATED_BODY()
+
+protected:
+    virtual void UpdateItemConfigs(FTableRowBase* TableRow) override;
+
+    void UpdateActorItemConfig(FTableRowBase* TableRow);
+};
