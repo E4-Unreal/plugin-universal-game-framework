887cd601e7863bd2382a52c5ce6f1de45d9afd17
이정훈 <eu4ng97@gmail.com>
2025-04-14 20:45:14 +0900

    Chore: ItemSystemEditor 모듈 생성

---
+++ Source/ItemSystemEditor/Private/ItemSystemEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "ItemSystemEditor.h"
+
+#define LOCTEXT_NAMESPACE "FItemSystemEditorModule"
+
+void FItemSystemEditorModule::StartupModule()
+{
+    
+}
+
+void FItemSystemEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FItemSystemEditorModule, ItemSystemEditor)
