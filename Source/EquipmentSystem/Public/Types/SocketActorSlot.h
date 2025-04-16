01c40511c7b8b321dec64debce2e9e9c3f97abde
이정훈 <eu4ng97@gmail.com>
2025-04-17 06:13:13 +0900

    Feat: SocketActorSlot 구조체 작성

---
+++ Source/EquipmentSystem/Public/Types/SocketActorSlot.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "SocketActorSlot.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FSocketActorSlot
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FGameplayTag SocketTag;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<AActor> Actor;
+};
