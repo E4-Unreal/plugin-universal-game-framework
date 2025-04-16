1b63472a7d4852ab060e02da9b84d0f8e4f7559c
이정훈 <eu4ng97@gmail.com>
2025-04-17 01:02:20 +0900

    Feat: SocketMeshActor 클래스 작성

---
+++ Source/EquipmentSystem/Private/Actors/SocketMeshActor.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/SocketMeshActor.h"
+
+const FName ASocketMeshActor::DefaultSceneRootName(TEXT("DefaultSceneRoot"));
+const FName ASocketMeshActor::SkeletalMeshComponentName(TEXT("SkeletalMesh"));
+const FName ASocketMeshActor::StaticMeshComponentName(TEXT("StaticMesh"));
+
+ASocketMeshActor::ASocketMeshActor(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    bReplicates = true;
+
+    /* DefaultSceneRoot */
+    DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(DefaultSceneRootName);
+    SetRootComponent(DefaultSceneRoot);
+
+    /* SkeletalMeshComponent */
+    SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(SkeletalMeshComponentName);
+    SkeletalMeshComponent->SetupAttachment(RootComponent);
+    SkeletalMeshComponent->SetIsReplicated(true);
+    SkeletalMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+
+    /* StaticMeshComponent */
+    StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(StaticMeshComponentName);
+    StaticMeshComponent->SetupAttachment(RootComponent);
+    StaticMeshComponent->SetIsReplicated(true);
+    StaticMeshComponent->SetMobility(EComponentMobility::Movable);
+    StaticMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+}
+
+void ASocketMeshActor::SetSkeletalMesh(USkeletalMesh* SkeletalMesh)
+{
+    if (SkeletalMeshComponent) SkeletalMeshComponent->SetSkeletalMesh(SkeletalMesh);
+}
+
+void ASocketMeshActor::SetStaticMesh(UStaticMesh* StaticMesh)
+{
+    if (StaticMeshComponent) StaticMeshComponent->SetStaticMesh(StaticMesh);
+}
+
+void ASocketMeshActor::SetBodyInstance(const FBodyInstance& OverrideBodyInstance)
+{
+    FName OverrideCollisionProfileName = OverrideBodyInstance.GetCollisionProfileName();
+    if (OverrideCollisionProfileName != UCollisionProfile::CustomCollisionProfileName && OverrideCollisionProfileName == SkeletalMeshComponent->GetCollisionProfileName()) return;
+
+    NetMulticast_SetBodyInstance(OverrideBodyInstance);
+}
+
+void ASocketMeshActor::NetMulticast_SetBodyInstance_Implementation(const FBodyInstance& OverrideBodyInstance)
+{
+    InternalSetBodyInstance(OverrideBodyInstance);
+}
+
+void ASocketMeshActor::InternalSetBodyInstance(const FBodyInstance& OverrideBodyInstance)
+{
+    if (OverrideBodyInstance.GetCollisionProfileName() == UCollisionProfile::CustomCollisionProfileName)
+    {
+        SkeletalMeshComponent->SetCollisionEnabled(OverrideBodyInstance.GetCollisionEnabled());
+        SkeletalMeshComponent->SetCollisionObjectType(OverrideBodyInstance.GetObjectType());
+        SkeletalMeshComponent->SetCollisionResponseToChannels(OverrideBodyInstance.GetResponseToChannels());
+
+        StaticMeshComponent->SetCollisionEnabled(OverrideBodyInstance.GetCollisionEnabled());
+        StaticMeshComponent->SetCollisionObjectType(OverrideBodyInstance.GetObjectType());
+        StaticMeshComponent->SetCollisionResponseToChannels(OverrideBodyInstance.GetResponseToChannels());
+    }
+    else
+    {
+        SkeletalMeshComponent->SetCollisionProfileName(OverrideBodyInstance.GetCollisionProfileName());
+
+        StaticMeshComponent->SetCollisionProfileName(OverrideBodyInstance.GetCollisionProfileName());
+    }
+}
---
+++ Source/EquipmentSystem/Public/Actors/SocketMeshActor.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SocketMeshActor.generated.h"
+
+UCLASS()
+class EQUIPMENTSYSTEM_API ASocketMeshActor : public AActor
+{
+    GENERATED_BODY()
+
+protected:
+    static const FName DefaultSceneRootName;
+    static const FName SkeletalMeshComponentName;
+    static const FName StaticMeshComponentName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USceneComponent> DefaultSceneRoot;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
+public:
+    ASocketMeshActor(const FObjectInitializer& ObjectInitializer);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetSkeletalMesh(USkeletalMesh* SkeletalMesh);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetStaticMesh(UStaticMesh* StaticMesh);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetBodyInstance(const FBodyInstance& OverrideBodyInstance);
+
+protected:
+    UFUNCTION(NetMulticast, Reliable)
+    void NetMulticast_SetBodyInstance(const FBodyInstance& OverrideBodyInstance);
+
+    virtual void InternalSetBodyInstance(const FBodyInstance& OverrideBodyInstance);
+
+public:
+    FORCEINLINE USkeletalMeshComponent* GetSkeletalMeshComponent() const { return SkeletalMeshComponent; }
+    FORCEINLINE UStaticMeshComponent* GetStaticMeshComponent() const { return StaticMeshComponent; }
+};
