1f7618cb01a140f6002466950aa0ecb4fcadf743
이정훈 <eu4ng97@gmail.com>
2025-04-17 01:57:05 +0900

    Fix: SocketMeshActor 리플리케이트 설정

--- Source/EquipmentSystem/Private/Actors/SocketMeshActor.cpp
+++ Source/EquipmentSystem/Private/Actors/SocketMeshActor.cpp
@@ -3,6 +3,8 @@
 
 #include "Actors/SocketMeshActor.h"
 
+#include "Net/UnrealNetwork.h"
+
 const FName ASocketMeshActor::DefaultSceneRootName(TEXT("DefaultSceneRoot"));
 const FName ASocketMeshActor::SkeletalMeshComponentName(TEXT("SkeletalMesh"));
 const FName ASocketMeshActor::StaticMeshComponentName(TEXT("StaticMesh"));
@@ -11,6 +13,7 @@ ASocketMeshActor::ASocketMeshActor(const FObjectInitializer& ObjectInitializer)
     : Super(ObjectInitializer)
 {
     bReplicates = true;
+    BodyInstance.SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
 
     /* DefaultSceneRoot */
     DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(DefaultSceneRootName);
@@ -20,14 +23,21 @@ ASocketMeshActor::ASocketMeshActor(const FObjectInitializer& ObjectInitializer)
     SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(SkeletalMeshComponentName);
     SkeletalMeshComponent->SetupAttachment(RootComponent);
     SkeletalMeshComponent->SetIsReplicated(true);
-    SkeletalMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
 
     /* StaticMeshComponent */
     StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(StaticMeshComponentName);
     StaticMeshComponent->SetupAttachment(RootComponent);
     StaticMeshComponent->SetIsReplicated(true);
     StaticMeshComponent->SetMobility(EComponentMobility::Movable);
-    StaticMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+
+    ApplyBodyInstance();
+}
+
+void ASocketMeshActor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(ThisClass, BodyInstance);
 }
 
 void ASocketMeshActor::SetSkeletalMesh(USkeletalMesh* SkeletalMesh)
@@ -40,35 +50,33 @@ void ASocketMeshActor::SetStaticMesh(UStaticMesh* StaticMesh)
     if (StaticMeshComponent) StaticMeshComponent->SetStaticMesh(StaticMesh);
 }
 
-void ASocketMeshActor::SetBodyInstance(const FBodyInstance& OverrideBodyInstance)
+void ASocketMeshActor::SetBodyInstance(const FBodyInstance& Value)
 {
-    FName OverrideCollisionProfileName = OverrideBodyInstance.GetCollisionProfileName();
-    if (OverrideCollisionProfileName != UCollisionProfile::CustomCollisionProfileName && OverrideCollisionProfileName == SkeletalMeshComponent->GetCollisionProfileName()) return;
-
-    NetMulticast_SetBodyInstance(OverrideBodyInstance);
+    BodyInstance = Value;
+    ApplyBodyInstance();
 }
 
-void ASocketMeshActor::NetMulticast_SetBodyInstance_Implementation(const FBodyInstance& OverrideBodyInstance)
+void ASocketMeshActor::ApplyBodyInstance() const
 {
-    InternalSetBodyInstance(OverrideBodyInstance);
-}
-
-void ASocketMeshActor::InternalSetBodyInstance(const FBodyInstance& OverrideBodyInstance)
-{
-    if (OverrideBodyInstance.GetCollisionProfileName() == UCollisionProfile::CustomCollisionProfileName)
+    if (BodyInstance.GetCollisionProfileName() == UCollisionProfile::CustomCollisionProfileName)
     {
-        SkeletalMeshComponent->SetCollisionEnabled(OverrideBodyInstance.GetCollisionEnabled());
-        SkeletalMeshComponent->SetCollisionObjectType(OverrideBodyInstance.GetObjectType());
-        SkeletalMeshComponent->SetCollisionResponseToChannels(OverrideBodyInstance.GetResponseToChannels());
+        SkeletalMeshComponent->SetCollisionEnabled(BodyInstance.GetCollisionEnabled());
+        SkeletalMeshComponent->SetCollisionObjectType(BodyInstance.GetObjectType());
+        SkeletalMeshComponent->SetCollisionResponseToChannels(BodyInstance.GetResponseToChannels());
 
-        StaticMeshComponent->SetCollisionEnabled(OverrideBodyInstance.GetCollisionEnabled());
-        StaticMeshComponent->SetCollisionObjectType(OverrideBodyInstance.GetObjectType());
-        StaticMeshComponent->SetCollisionResponseToChannels(OverrideBodyInstance.GetResponseToChannels());
+        StaticMeshComponent->SetCollisionEnabled(BodyInstance.GetCollisionEnabled());
+        StaticMeshComponent->SetCollisionObjectType(BodyInstance.GetObjectType());
+        StaticMeshComponent->SetCollisionResponseToChannels(BodyInstance.GetResponseToChannels());
     }
     else
     {
-        SkeletalMeshComponent->SetCollisionProfileName(OverrideBodyInstance.GetCollisionProfileName());
+        SkeletalMeshComponent->SetCollisionProfileName(BodyInstance.GetCollisionProfileName());
 
-        StaticMeshComponent->SetCollisionProfileName(OverrideBodyInstance.GetCollisionProfileName());
+        StaticMeshComponent->SetCollisionProfileName(BodyInstance.GetCollisionProfileName());
     }
+}
+
+void ASocketMeshActor::OnRep_BodyInstance()
+{
+    ApplyBodyInstance();
 }
--- Source/EquipmentSystem/Public/Actors/SocketMeshActor.h
+++ Source/EquipmentSystem/Public/Actors/SocketMeshActor.h
@@ -26,8 +26,14 @@ private:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
     TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
 
+protected:
+    UPROPERTY(ReplicatedUsing = OnRep_BodyInstance, Transient)
+    FBodyInstance BodyInstance;
+
 public:
     ASocketMeshActor(const FObjectInitializer& ObjectInitializer);
+
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
     UFUNCTION(BlueprintCallable)
     virtual void SetSkeletalMesh(USkeletalMesh* SkeletalMesh);
@@ -36,13 +42,13 @@ public:
     virtual void SetStaticMesh(UStaticMesh* StaticMesh);
 
     UFUNCTION(BlueprintCallable)
-    virtual void SetBodyInstance(const FBodyInstance& OverrideBodyInstance);
+    virtual void SetBodyInstance(const FBodyInstance& Value);
 
 protected:
-    UFUNCTION(NetMulticast, Reliable)
-    void NetMulticast_SetBodyInstance(const FBodyInstance& OverrideBodyInstance);
+    void ApplyBodyInstance() const;
 
-    virtual void InternalSetBodyInstance(const FBodyInstance& OverrideBodyInstance);
+    UFUNCTION()
+    virtual void OnRep_BodyInstance();
 
 public:
     FORCEINLINE USkeletalMeshComponent* GetSkeletalMeshComponent() const { return SkeletalMeshComponent; }
