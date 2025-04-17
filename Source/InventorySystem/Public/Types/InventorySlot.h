f80c0c36d8b911c88834dcc2c0d5b4ecf1fcc4ab
이정훈 <eu4ng97@gmail.com>
2025-04-17 16:43:48 +0900

    Feat: InventorySlot 구조체 작성

---
+++ Source/InventorySystem/Public/Types/InventorySlot.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventorySlot.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FInventorySlot
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Index;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UObject> Item;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Quantity;
+};
