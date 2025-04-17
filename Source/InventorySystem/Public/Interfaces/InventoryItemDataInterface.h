07d4d2268caaf000b63b4e7901e41ae52018bd36
이정훈 <eu4ng97@gmail.com>
2025-04-17 16:59:54 +0900

    Feat: InventoryItemDataInterface 인터페이스 작성

---
+++ Source/InventorySystem/Public/Interfaces/InventoryItemDataInterface.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InventoryItemDataInterface.generated.h"
+
+struct FInventoryItemData;
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInventoryItemDataInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class INVENTORYSYSTEM_API IInventoryItemDataInterface
+{
+    GENERATED_BODY()
+
+    // Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    const FInventoryItemData& GetInventoryItemData() const;
+    virtual const FInventoryItemData& GetInventoryItemData_Implementation() const;
+};
