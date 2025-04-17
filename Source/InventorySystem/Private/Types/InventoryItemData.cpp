06df085a969baaae7ca3d7ab27f54e3c44101512
이정훈 <eu4ng97@gmail.com>
2025-04-17 16:48:23 +0900

    Feat: InventoryItemData 구조체 작성

---
+++ Source/InventorySystem/Private/Types/InventoryItemData.cpp
@@ -0,0 +1,6 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Types/InventoryItemData.h"
+
+const FInventoryItemData FInventoryItemData::Empty{ -1, false, false };
---
+++ Source/InventorySystem/Public/Types/InventoryItemData.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "InventoryItemData.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FInventoryItemData
+{
+    GENERATED_BODY()
+
+    static const FInventoryItemData Empty;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 1))
+    int32 MaxStack = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    bool bCanEquip = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    bool bCanUse = false;
+
+    FORCEINLINE bool IsValid() const { return MaxStack > 0; }
+    FORCEINLINE bool IsNotValid() const { return !IsValid(); }
+};
