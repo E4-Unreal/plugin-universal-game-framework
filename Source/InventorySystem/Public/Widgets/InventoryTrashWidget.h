af736bfb627dc9a40352409d8d0ad8381c3ed7cc
이정훈 <eu4ng97@gmail.com>
2025-04-19 08:45:38 +0900

    Feat: InventoryTrashWidget 클래스 작성

---
+++ Source/InventorySystem/Public/Widgets/InventoryTrashWidget.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryWidgetBase.h"
+#include "InventoryTrashWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInventoryTrashWidget : public UInventoryWidgetBase
+{
+    GENERATED_BODY()
+
+protected:
+    virtual void OnInventorySlotWidgetDrop(UInventorySlotWidgetBase* InventorySlotWidget) override;
+};
