5a903fe882ccfa9a20c64e0a727ba5a6afe7e52f
이정훈 <eu4ng97@gmail.com>
2025-04-19 07:48:30 +0900

    Feat: StorageWidget 클래스 작성

---
+++ Source/InventorySystem/Private/Widgets/StorageWidget.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/StorageWidget.h"
+
+#include "Components/InventoryComponent.h"
+#include "Widgets/InventoryPanelWidget.h"
+
+void UStorageWidget::SetTargetActor(AActor* NewTargetActor)
+{
+    if (TargetActor != NewTargetActor)
+    {
+        OnTargetActorChanged(TargetActor, NewTargetActor);
+        TargetActor = NewTargetActor;
+    }
+}
+
+void UStorageWidget::OnTargetActorChanged(AActor* OldTargetActor, AActor* NewTargetActor)
+{
+    GetStoragePanel()->SetInventoryComponent(NewTargetActor->GetComponentByClass<UInventoryComponent>());
+}
