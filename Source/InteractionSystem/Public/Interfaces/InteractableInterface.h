a5f93d827da93fb90f94c9c711906e1e6a2ddde8
이정훈 <eu4ng97@gmail.com>
2025-04-19 10:31:45 +0900

    Feat: InteractableInterface 인터페이스 작성

---
+++ Source/InteractionSystem/Public/Interfaces/InteractableInterface.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InteractableInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, BlueprintType, Blueprintable)
+class UInteractableInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class INTERACTIONSYSTEM_API IInteractableInterface
+{
+    GENERATED_BODY()
+
+    // Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void TryInteract(AActor* Interactor);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void CancelInteract(AActor* Interactor);
+};
