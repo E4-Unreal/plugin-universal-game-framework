61ab611589b7fe3bef9d69e10b73f7218b1907d0
이정훈 <eu4ng97@gmail.com>
2025-04-27 08:00:20 +0900

    Feat: StorageActor 클래스 작성

---
+++ Source/InventorySystem/Private/Actors/StorageActor.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/StorageActor.h"
+
+#include "Components/InventoryComponent.h"
+
+const FName AStorageActor::InventoryName(TEXT("Inventory"));
+
+AStorageActor::AStorageActor(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    /* Inventory */
+    Inventory = CreateDefaultSubobject<UInventoryComponent>(InventoryName);
+    Inventory->SetMaxSlotNum(10);
+}
---
+++ Source/InventorySystem/Public/Actors/StorageActor.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/InteractableActor.h"
+#include "GameFramework/Actor.h"
+#include "StorageActor.generated.h"
+
+class UInventoryComponent;
+
+UCLASS()
+class INVENTORYSYSTEM_API AStorageActor : public AInteractableActor
+{
+    GENERATED_BODY()
+
+protected:
+    const static FName InventoryName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSubclassOf<UUserWidget> StorageWidgetClass;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInventoryComponent> Inventory;
+
+public:
+    AStorageActor(const FObjectInitializer& ObjectInitializer);
+
+public:
+    FORCEINLINE UInventoryComponent* GetInventory() const { return Inventory; }
+};
