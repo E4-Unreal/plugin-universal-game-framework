c97db3bb34d81724bfe7ad9a28b146c2e331fa4b
이정훈 <eu4ng97@gmail.com>
2025-05-01 15:03:54 +0900

    Feat: InventorySystemFunctionLibrary 클래스 작성

---
+++ Source/InventorySystem/Private/InventorySystemFunctionLibrary.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventorySystemFunctionLibrary.h"
+
+#include "Interfaces/ItemActorInterface.h"
+
+AActor* UInventorySystemFunctionLibrary::SpawnItemActor(AActor* Owner, TSubclassOf<AActor> ItemActorClass,
+                                                     const TArray<FInventoryItem>& InventoryItems, const FVector& Offset)
+{
+    if (bool bCanSpawn = Owner && ItemActorClass && ItemActorClass->ImplementsInterface(UItemActorInterface::StaticClass()) && !InventoryItems.IsEmpty(); !bCanSpawn) return nullptr;
+
+    UWorld* World = Owner->GetWorld();
+    if (!World) return nullptr;
+
+    FTransform SpawnTransform = Owner->GetActorTransform();
+    SpawnTransform.SetLocation(SpawnTransform.GetLocation() + Offset);
+    AActor* SpawnedItemActor = World->SpawnActorDeferred<AActor>(ItemActorClass, SpawnTransform, Owner, Owner->GetInstigator(), ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn);
+    IItemActorInterface::Execute_SetInventoryItems(SpawnedItemActor, InventoryItems);
+    SpawnedItemActor->FinishSpawning(SpawnTransform);
+
+    return SpawnedItemActor;
+}
