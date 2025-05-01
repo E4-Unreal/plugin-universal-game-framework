817e0dbebfb97571d2130c3bbad107dbf47f155f
이정훈 <eu4ng97@gmail.com>
2025-05-01 14:15:42 +0900

    Feat: ItemActorInterface 인터페이스 작성

---
+++ Source/InventorySystem/Private/Interfaces/ItemActorInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/ItemActorInterface.h"
+
+
+// Add default functionality here for any IItemActorInterface functions that are not pure virtual.
