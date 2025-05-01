09401884d6f09ccdb7a00206290a9d9a1f434f2b
이정훈 <eu4ng97@gmail.com>
2025-05-01 17:36:01 +0900

    Feat: InventoryOutsideWidget 클래스 작성

---
+++ Source/InventorySystem/Private/Widgets/InventoryOutsideWidget.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/InventoryOutsideWidget.h"
+
+#include "Components/InventoryComponent.h"
+#include "Widgets/InventorySlotWidgetBase.h"
+
+void UInventoryOutsideWidget::OnInventorySlotWidgetDrop(UInventorySlotWidgetBase* InventorySlotWidget)
+{
+    Super::OnInventorySlotWidgetDrop(InventorySlotWidget);
+
+    auto InventoryComponent = InventorySlotWidget->GetInventoryComponent();
+    int32 SlotIndex = InventorySlotWidget->GetSlotIndex();
+    const auto& InventorySlot = InventoryComponent->GetInventorySlot(SlotIndex);
+    InventoryComponent->DropItemFromSlot(SlotIndex, InventorySlot.Quantity);
+}
---
+++ Source/InventorySystem/Public/Widgets/InventoryOutsideWidget.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryWidgetBase.h"
+#include "InventoryOutsideWidget.generated.h"
+
+/**
+ * 인벤토리 아이템 드랍을 위한 배경 위젯 클래스
+ */
+UCLASS(Abstract)
+class INVENTORYSYSTEM_API UInventoryOutsideWidget : public UInventoryWidgetBase
+{
+    GENERATED_BODY()
+
+protected:
+    virtual void OnInventorySlotWidgetDrop(UInventorySlotWidgetBase* InventorySlotWidget) override;
+};
