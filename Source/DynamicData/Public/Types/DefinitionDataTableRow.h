5b4eb746aa9cfa39f8486c215c0e6fee2fb59c31
이정훈 <eu4ng97@gmail.com>
2025-05-10 02:12:36 +0900

    Feat: DefinitionDataTableRow 구조체 작성

---
+++ Source/DynamicData/Public/Types/DefinitionDataTableRow.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "DefinitionDataTableRow.generated.h"
+
+USTRUCT()
+struct FDefinitionDataTableRow : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 ID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText DisplayText;
+};
