174cfda446ac507405315eb19e513f6ef3d5b0b9
이정훈 <eu4ng97@gmail.com>
2025-05-10 00:25:45 +0900

    Feat: DynamicDataSubsystem 클래스 작성

---
+++ Source/DynamicData/Public/Subsystems/DynamicDataSubsystem.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "DynamicDataSubsystem.generated.h"
+
+class UDefinitionBase;
+
+/**
+ *
+ */
+UCLASS()
+class DYNAMICDATA_API UDynamicDataSubsystem : public UEngineSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    /* Subsystem */
+
+    virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+    UFUNCTION(BlueprintCallable)
+    virtual UDefinitionBase* GetDefinition(TSubclassOf<UDefinitionBase> DefinitionClass, int32 ID);
+
+    template <typename T = UDefinitionBase>
+    T* GetDefinition(int32 ID)
+    {
+        return Cast<T>(GetDefinition(T::StaticClass(), ID));
+    }
+};
