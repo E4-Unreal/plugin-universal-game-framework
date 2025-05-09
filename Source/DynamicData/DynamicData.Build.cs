522922ee6061cc774cfca0d7acd1a39094b6e390
이정훈 <eu4ng97@gmail.com>
2025-05-09 17:25:16 +0900

    Chore: DynamicData 모듈 생성

---
+++ Source/DynamicData/DynamicData.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class DynamicData : ModuleRules
+{
+    public DynamicData(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/DynamicData/Private/DynamicData.cpp
@@ -0,0 +1,17 @@
+﻿#include "DynamicData.h"
+
+#define LOCTEXT_NAMESPACE "FDynamicDataModule"
+
+void FDynamicDataModule::StartupModule()
+{
+    
+}
+
+void FDynamicDataModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDynamicDataModule, DynamicData)
