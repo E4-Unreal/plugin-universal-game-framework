c78f4ce508923531688a84e07be8c195bdebb1d2
이정훈 <eu4ng97@gmail.com>
2025-05-10 03:09:39 +0900

    Chore: UniversalGameFrameworkEditor 모듈 생성

---
+++ Source/UniversalGameFrameworkEditor/Private/UniversalGameFrameworkEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "UniversalGameFrameworkEditor.h"
+
+#define LOCTEXT_NAMESPACE "FUniversalGameFrameworkEditorModule"
+
+void FUniversalGameFrameworkEditorModule::StartupModule()
+{
+    
+}
+
+void FUniversalGameFrameworkEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUniversalGameFrameworkEditorModule, UniversalGameFrameworkEditor)
---
+++ Source/UniversalGameFrameworkEditor/UniversalGameFrameworkEditor.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class UniversalGameFrameworkEditor : ModuleRules
+{
+    public UniversalGameFrameworkEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
