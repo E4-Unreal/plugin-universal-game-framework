81e6a79314486a37b4b1772e15b18ccfe61b02b9
이정훈 <eu4ng97@gmail.com>
2025-06-07 19:40:04 +0900

    Feat: ShopGameplayTags 파일 작성

---
+++ Source/ShopSystem/Private/ShopGameplayTags.cpp
@@ -0,0 +1,10 @@
+﻿#include "ShopGameplayTags.h"
+
+namespace ShopGameplayTags
+{
+    namespace Currency
+    {
+        UE_DEFINE_GAMEPLAY_TAG(Root, "Currency")
+        UE_DEFINE_GAMEPLAY_TAG(Default, "Currency.Default")
+    }
+}
---
+++ Source/ShopSystem/Public/ShopGameplayTags.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ShopGameplayTags
+{
+    namespace Currency
+    {
+        SHOPSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+        SHOPSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Default)
+    }
+}
+
+using namespace ShopGameplayTags;
