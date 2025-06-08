f1f03e94184bad913b9e60b8f4b823ddef040e29
이정훈 <eu4ng97@gmail.com>
2025-06-08 12:54:35 +0900

    Feat: CurrencyManagerComponent 클래스 작성

---
+++ Source/ShopSystem/Private/Components/CurrencyManagerComponent.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/CurrencyManagerComponent.h"
+
+
+UCurrencyManagerComponent::UCurrencyManagerComponent()
+{
+    bWantsInitializeComponent = true;
+}
+
+void UCurrencyManagerComponent::InitializeComponent()
+{
+    Super::InitializeComponent();
+
+    for (const FGameplayTag& CurrencyType : CurrencyTypes)
+    {
+        CurrencyMap.Emplace(CurrencyType, 0);
+    }
+}
+
+int32 UCurrencyManagerComponent::GetCurrency(const FGameplayTag& CurrencyType) const
+{
+    const int32* CurrencyPtr = CurrencyMap.Find(CurrencyType);
+    return CurrencyPtr ? *CurrencyPtr : 0;
+}
+
+bool UCurrencyManagerComponent::AddCurrency(const FGameplayTag& CurrencyType, int32 Amount)
+{
+    if (bool bCanAdd = Amount > 0 && CurrencyMap.Contains(CurrencyType); !bCanAdd) return false;
+
+    CurrencyMap.Emplace(CurrencyType, GetCurrency(CurrencyType) + Amount);
+
+    return true;
+}
+
+bool UCurrencyManagerComponent::RemoveCurrency(const FGameplayTag& CurrencyType, int32 Amount)
+{
+    if (bool bCanRemove = Amount > 0 && GetCurrency(CurrencyType) > Amount; !bCanRemove) return false;
+
+    CurrencyMap.Emplace(CurrencyType, GetCurrency(CurrencyType) - Amount);
+
+    return true;
+}
---
+++ Source/ShopSystem/Public/Components/CurrencyManagerComponent.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "CurrencyManagerComponent.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class SHOPSYSTEM_API UCurrencyManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (Categories = "Currency"))
+    FGameplayTagContainer CurrencyTypes;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TMap<FGameplayTag, int32> CurrencyMap;
+
+public:
+    UCurrencyManagerComponent();
+
+    virtual void InitializeComponent() override;
+
+    UFUNCTION(BlueprintPure)
+    virtual int32 GetCurrency(const FGameplayTag& CurrencyType) const;
+
+    UFUNCTION(BlueprintPure)
+    virtual bool HasCurrency(const FGameplayTag& CurrencyType, int32 Amount) const { return GetCurrency(CurrencyType) >= Amount; }
+
+    UFUNCTION(BlueprintCallable)
+    virtual bool AddCurrency(const FGameplayTag& CurrencyType, int32 Amount);
+
+    UFUNCTION(BlueprintCallable)
+    virtual bool RemoveCurrency(const FGameplayTag& CurrencyType, int32 Amount);
+};
