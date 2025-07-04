f13b13e87e120b27d2fcb3e1bb4f3b597dcf64c0
이정훈 <eu4ng97@gmail.com>
2025-07-05 03:31:18 +0900

    Chore: WidgetFramework 모듈 생성

---
+++ Source/WidgetFramework/Private/WidgetFramework.cpp
@@ -0,0 +1,17 @@
+﻿#include "WidgetFramework.h"
+
+#define LOCTEXT_NAMESPACE "FWidgetFrameworkModule"
+
+void FWidgetFrameworkModule::StartupModule()
+{
+    
+}
+
+void FWidgetFrameworkModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FWidgetFrameworkModule, WidgetFramework)
