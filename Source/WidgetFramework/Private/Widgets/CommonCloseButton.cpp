d305c4fed5d55bb334453972c4a9023161f3644c
이정훈 <eu4ng97@gmail.com>
2025-07-05 07:35:05 +0900

    Feat: CommonCloseButton 클래스 작성

---
+++ Source/WidgetFramework/Private/Widgets/CommonCloseButton.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/CommonCloseButton.h"
+
+#include "CommonUIUtils.h"
+
+UCommonCloseButton::UCommonCloseButton(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    ButtonText = FText::FromString(TEXT("X"));
+}
+
+void UCommonCloseButton::NativeOnClicked()
+{
+    Super::NativeOnClicked();
+
+    UUserWidget* OwningUserWidget = CommonUIUtils::GetOwningUserWidget(this);
+    OwningUserWidget->RemoveFromParent();
+}
---
+++ Source/WidgetFramework/Public/Widgets/CommonCloseButton.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTextButton.h"
+#include "CommonCloseButton.generated.h"
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class WIDGETFRAMEWORK_API UCommonCloseButton : public UCommonTextButton
+{
+    GENERATED_BODY()
+
+public:
+    UCommonCloseButton(const FObjectInitializer& ObjectInitializer);
+
+protected:
+    virtual void NativeOnClicked() override;
+};
