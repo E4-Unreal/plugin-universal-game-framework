d3d243f465cd56d19966bf4b8b9ac46beca85a6a
이정훈 <eu4ng97@gmail.com>
2025-07-05 04:24:15 +0900

    Feat: WidgetLevelActor 클래스 작성

---
+++ Source/WidgetFramework/Private/Levels/WidgetLevelActor.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Levels/WidgetLevelActor.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AWidgetLevelActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+    CreateWidgets();
+    ShowWidgets();
+}
+
+void AWidgetLevelActor::CreateWidgets()
+{
+    RemoveWidgets();
+    Widgets.Reserve(WidgetClasses.Num());
+
+    for (TSubclassOf<UUserWidget> WidgetClass : WidgetClasses)
+    {
+        if (!WidgetClass) continue;
+
+        UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), WidgetClass);
+        Widgets.Emplace(Widget);
+    }
+}
+
+void AWidgetLevelActor::RemoveWidgets()
+{
+    HideWidgets();
+
+    Widgets.Reset();
+}
+
+void AWidgetLevelActor::ToggleWidgets()
+{
+    if (!bVisible)
+    {
+        ShowWidgets();
+    }
+    else
+    {
+        HideWidgets();
+    }
+}
+
+void AWidgetLevelActor::ShowWidgets()
+{
+    if (bVisible) return;
+    bVisible = true;
+
+    for (TObjectPtr<UUserWidget> Widget : Widgets)
+    {
+        Widget->AddToViewport();
+    }
+}
+
+void AWidgetLevelActor::HideWidgets()
+{
+    if (!bVisible) return;
+    bVisible = false;
+
+    for (TObjectPtr<UUserWidget> Widget : Widgets)
+    {
+        Widget->RemoveFromParent();
+    }
+}
---
+++ Source/WidgetFramework/Public/Levels/WidgetLevelActor.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "WidgetLevelActor.generated.h"
+
+UCLASS()
+class WIDGETFRAMEWORK_API AWidgetLevelActor : public ALevelScriptActor
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TArray<TSubclassOf<UUserWidget>> WidgetClasses;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    bool bVisible;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TArray<TObjectPtr<UUserWidget>> Widgets;
+
+protected:
+    virtual void BeginPlay() override;
+
+    UFUNCTION(BlueprintCallable)
+    void CreateWidgets();
+
+    UFUNCTION(BlueprintCallable)
+    void RemoveWidgets();
+
+    UFUNCTION(BlueprintCallable)
+    void ToggleWidgets();
+
+    UFUNCTION(BlueprintCallable)
+    void ShowWidgets();
+
+    UFUNCTION(BlueprintCallable)
+    void HideWidgets();
+};
