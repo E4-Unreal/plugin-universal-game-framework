0139564d1bb9cf6a85ee6528cb8105310a8a6615
이정훈 <eu4ng97@gmail.com>
2025-08-08 17:00:15 +0900

    Chore: WidgetManager 모듈 추가

---
+++ Source/WidgetManager/WidgetManager.Build.cs
@@ -0,0 +1,28 @@
+﻿using UnrealBuildTool;
+
+public class WidgetManager : ModuleRules
+{
+    public WidgetManager(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UMG",
+                "CommonUI"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
