8a16ee0fdd59fdce76be7b1bf110d77d2023e606
이정훈 <eu4ng97@gmail.com>
2025-08-08 17:13:08 +0900

    Feat: WidgetManagerComponent 클래스 작성 및 StartupWidget 설정 기능 추가

---
+++ Source/WidgetManager/Private/Components/WidgetManagerComponent.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/WidgetManagerComponent.h"
+
+#include "Blueprint/UserWidget.h"
+
+
+UWidgetManagerComponent::UWidgetManagerComponent(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+}
+
+void UWidgetManagerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    CreateStartupWidgets();
+    ShowStartupWidgets();
+}
+
+void UWidgetManagerComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+    HideStartupWidgets();
+    RemoveStartupWidgets();
+
+    Super::OnComponentDestroyed(bDestroyingHierarchy);
+}
+
+void UWidgetManagerComponent::CreateStartupWidgets()
+{
+    if (!StartupWidgets.IsEmpty()) return;
+
+    StartupWidgets.Reserve(StartupWidgetClasses.Num());
+    for (TSubclassOf<UUserWidget> StartupWidgetClass : StartupWidgetClasses)
+    {
+        if (StartupWidgetClass)
+        {
+            UUserWidget* StartupWidget = CreateWidget<UUserWidget>(GetWorld(), StartupWidgetClass);
+            StartupWidgets.Emplace(StartupWidget);
+        }
+    }
+}
+
+void UWidgetManagerComponent::RemoveStartupWidgets()
+{
+    StartupWidgets.Empty();
+}
+
+void UWidgetManagerComponent::ShowStartupWidgets()
+{
+    for (UUserWidget* StartupWidget : StartupWidgets)
+    {
+        if (StartupWidget && !StartupWidget->IsInViewport())
+        {
+            StartupWidget->AddToViewport();
+        }
+    }
+}
+
+void UWidgetManagerComponent::HideStartupWidgets()
+{
+    for (UUserWidget* StartupWidget : StartupWidgets)
+    {
+        if (StartupWidget && StartupWidget->IsInViewport())
+        {
+            StartupWidget->RemoveFromParent();
+        }
+    }
+}
---
+++ Source/WidgetManager/Public/Components/WidgetManagerComponent.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WidgetManagerComponent.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class WIDGETMANAGER_API UWidgetManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSet<TSubclassOf<UUserWidget>> StartupWidgetClasses;
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TArray<TObjectPtr<UUserWidget>> StartupWidgets;
+
+public:
+    UWidgetManagerComponent(const FObjectInitializer& ObjectInitializer);
+
+    virtual void BeginPlay() override;
+    virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
+protected:
+    void CreateStartupWidgets();
+    void RemoveStartupWidgets();
+    void ShowStartupWidgets();
+    void HideStartupWidgets();
+};
