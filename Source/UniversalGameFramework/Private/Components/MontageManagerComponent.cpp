50c0bc1d2915fc8e0bf2b18595595a5483263b20
이정훈 <eu4ng97@gmail.com>
2025-08-13 21:48:14 +0900

    Feat: MontageManagerComponent 클래스 생성

---
+++ Source/UniversalGameFramework/Private/Components/MontageManagerComponent.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/MontageManagerComponent.h"
+
+
+UMontageManagerComponent::UMontageManagerComponent(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+}
---
+++ Source/UniversalGameFramework/Public/Components/MontageManagerComponent.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MontageManagerComponent.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class UNIVERSALGAMEFRAMEWORK_API UMontageManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UMontageManagerComponent(const FObjectInitializer& ObjectInitializer);
+};
