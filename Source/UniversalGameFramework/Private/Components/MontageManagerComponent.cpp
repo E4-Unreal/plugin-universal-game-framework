4648a9366fcf964c9ec981fa78552a86615a995a
이정훈 <eu4ng97@gmail.com>
2025-08-13 22:39:01 +0900

    Feat: MontageManagerComponent 작성 (#29)
    * Feat: MontageManagerComponent 클래스 생성
    
    * Feat: PlayMontage 메서드 추가
    
    * Feat: PlayMontageByTag 메서드 추가
    
    * Feat: SetMesh 메서드 추가

---
+++ Source/UniversalGameFramework/Private/Components/MontageManagerComponent.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/MontageManagerComponent.h"
+
+#include "GameFramework/Character.h"
+
+
+UMontageManagerComponent::UMontageManagerComponent(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    bWantsInitializeComponent = true;
+}
+
+void UMontageManagerComponent::InitializeComponent()
+{
+    Super::InitializeComponent();
+
+    if (!Mesh.IsValid())
+    {
+        if (ACharacter* OwningCharacter = Cast<ACharacter>(GetOwner()))
+        {
+            Mesh = OwningCharacter->GetMesh();
+        }
+        else
+        {
+            Mesh = GetOwner()->GetComponentByClass<USkeletalMeshComponent>();
+        }
+    }
+}
+
+void UMontageManagerComponent::SetMesh(USkeletalMeshComponent* NewMesh)
+{
+    Mesh = NewMesh;
+}
+
+void UMontageManagerComponent::PlayMontage(UAnimMontage* Montage) const
+{
+    if (Montage && Mesh.IsValid())
+    {
+        if (UAnimInstance* AnimInstance = Mesh->GetAnimInstance())
+        {
+            AnimInstance->Montage_Play(Montage);
+        }
+    }
+}
+
+UAnimMontage* UMontageManagerComponent::GetMontageByTag(const FGameplayTag& Tag) const
+{
+    UAnimMontage* Montage = nullptr;
+    FGameplayTagContainer TagContainer = Tag.GetGameplayTagParents();
+    for (const FGameplayTag& ParentTag : TagContainer)
+    {
+        if (MontageMap.Contains(ParentTag))
+        {
+            Montage = MontageMap[ParentTag];
+            break;
+        }
+    }
+
+    return Montage;
+}
+
+void UMontageManagerComponent::PlayMontageByTag(const FGameplayTag& Tag) const
+{
+    PlayMontage(GetMontageByTag(Tag));
+}
---
+++ Source/UniversalGameFramework/Public/Components/MontageManagerComponent.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "MontageManagerComponent.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class UNIVERSALGAMEFRAMEWORK_API UMontageManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TMap<FGameplayTag, TObjectPtr<UAnimMontage>> MontageMap;
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TWeakObjectPtr<USkeletalMeshComponent> Mesh;
+
+public:
+    UMontageManagerComponent(const FObjectInitializer& ObjectInitializer);
+
+    virtual void InitializeComponent() override;
+
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetMesh(USkeletalMeshComponent* NewMesh);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void PlayMontage(UAnimMontage* Montage) const;
+
+    UFUNCTION(BlueprintPure)
+    virtual UAnimMontage* GetMontageByTag(const FGameplayTag& Tag) const;
+
+    UFUNCTION(BlueprintCallable)
+    virtual void PlayMontageByTag(const FGameplayTag& Tag) const;
+};
