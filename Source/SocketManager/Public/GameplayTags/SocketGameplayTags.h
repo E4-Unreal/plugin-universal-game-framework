14a7a7cf788a97a27137bcdc6e616f7297fdc962
이정훈 <eu4ng97@gmail.com>
2025-08-16 05:34:06 +0900

    Feat: SocketGameplayTags 작성

---
+++ Source/SocketManager/Public/GameplayTags/SocketGameplayTags.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace SocketGameplayTags
+{
+    namespace Socket
+    {
+        namespace Character
+        {
+            SOCKETMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+            SOCKETMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(RightHand)
+        }
+    }
+}
+
+using namespace SocketGameplayTags;
