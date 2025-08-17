966cd08ae8e6fed950ca692928a47762a4c8fc72
이정훈 <eu4ng97@gmail.com>
2025-08-18 03:12:45 +0900

    Refactor: EquipmentTypeTags 폴더 구조 변경 및 리팩토링

---
+++ Source/EquipmentManager/Private/GameplayTags/EquipmentTypeTags.cpp
@@ -0,0 +1,24 @@
+﻿#include "GameplayTags/EquipmentTypeTags.h"
+
+namespace EquipmentTypeTags
+{
+    namespace Equipment
+    {
+        UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Equipment", "장비")
+
+        namespace Weapon
+        {
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Equipment.Weapon", "무기")
+        }
+
+        namespace Armor
+        {
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Equipment.Armor", "방어구")
+        }
+
+        namespace Accessory
+        {
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Equipment.Accessory", "장신구")
+        }
+    }
+}
---
+++ Source/EquipmentManager/Public/GameplayTags/EquipmentTypeTags.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace EquipmentTypeTags
+{
+    namespace Equipment
+    {
+        EQUIPMENTMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+
+        namespace Weapon
+        {
+            EQUIPMENTMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+        }
+
+        namespace Armor
+        {
+            EQUIPMENTMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+        }
+
+        namespace Accessory
+        {
+            EQUIPMENTMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+        }
+    }
+}
+
+using namespace EquipmentTypeTags;
