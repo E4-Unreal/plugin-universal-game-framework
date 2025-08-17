3b3f20d05da626c6f7b4cd5193e04e4533ba7829
이정훈 <eu4ng97@gmail.com>
2025-08-18 02:45:12 +0900

    Feat: EquipmentActor 클래스에 StaticMesh, SkeletalMesh 컴포넌트 부착 및 초기화 기능 추가

--- Source/EquipmentManager/Public/Actors/EquipmentActor.h
+++ Source/EquipmentManager/Public/Actors/EquipmentActor.h
@@ -13,11 +13,32 @@ class EQUIPMENTMANAGER_API AEquipmentActor : public AActor, public IEquipmentAct
     GENERATED_BODY()
 
 protected:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (MustImplement = "EquipmentDataInterface"))
+    const static FName DefaultSceneName;
+    const static FName StaticMeshName;
+    const static FName SkeletalMeshName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USceneComponent> DefaultScene;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USkeletalMeshComponent> SkeletalMesh;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
     TObjectPtr<UDataAsset> EquipmentData;
 
 public:
     AEquipmentActor(const FObjectInitializer& ObjectInitializer);
+
+    virtual void PostInitializeComponents() override;
+
+#if WITH_EDITOR
+    virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
 
     /* EquipmentInterface */
 
@@ -25,4 +46,12 @@ public:
     virtual void SetEquipmentData_Implementation(UDataAsset* NewEquipmentData) override;
     virtual void Equip_Implementation(AActor* NewOwner) override;
     virtual void UnEquip_Implementation() override;
+
+protected:
+    virtual void InitializeByData();
+
+public:
+    FORCEINLINE USceneComponent* GetDefaultScene() const { return DefaultScene; }
+    FORCEINLINE UStaticMeshComponent* GetStaticMesh() const { return StaticMesh; }
+    FORCEINLINE USkeletalMeshComponent* GetSkeletonMesh() const { return SkeletalMesh; }
 };
