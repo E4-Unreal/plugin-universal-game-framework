e243cf3aa10e7a87b6c73336d7484d99460c1680
이정훈 <eu4ng97@gmail.com>
2025-08-21 14:52:27 +0900

    Feat: QuickSlotManager 전용 인터페이스 작성

---
+++ Source/QuickSlotManager/Public/Interfaces/QuickSlotDataInterface.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "QuickSlotDataInterface.generated.h"
+
+struct FGameplayTag;
+
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class UQuickSlotDataInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class QUICKSLOTMANAGER_API IQuickSlotDataInterface
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    TSubclassOf<AActor> GetActorClass() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    FGameplayTag GetSocketTag() const;
+};
