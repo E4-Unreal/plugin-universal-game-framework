c4ee1ca0863ec73cf528b5a1c3e7ee831dd02d2c
이정훈 <eu4ng97@gmail.com>
2025-08-22 01:33:52 +0900

    Feat: UGFQuickSlotPanelWidget 클래스 작성

---
+++ Source/UniversalGameFramework/Private/Widgets/QuickSlot/UGFQuickSlotPanelWidget.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/QuickSlot/UGFQuickSlotPanelWidget.h"
+
+#include "Components/QuickSlotManagerComponent.h"
+#include "Components/UniformGridPanel.h"
+#include "Widgets/QuickSlot/UGFQuickSlotWidget.h"
+
+void UUGFQuickSlotPanelWidget::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+    CreateSlotWidgets();
+}
+
+void UUGFQuickSlotPanelWidget::CreateSlotWidgets()
+{
+    // Destroy
+    for (int32 Index = 0; Index < SlotWidgetMap.Num(); ++Index)
+    {
+        UUGFQuickSlotWidget* SlotWidget = SlotWidgetMap[Index];
+        SlotWidget->RemoveFromParent();
+        QuickSlotPanel->RemoveChildAt(Index);
+    }
+    SlotWidgetMap.Reset();
+
+    // Create
+    int32 SlotNum = QuickSlotManager.IsValid() ? QuickSlotManager->SlotNum : PreviewSlotNum;
+    for (int32 Index = 0; Index < SlotNum; ++Index)
+    {
+        auto SlotWidget = CreateWidget<UUGFQuickSlotWidget>(this, SlotWidgetClass);
+        SlotWidget->SetSlotIndex(Index);
+
+        int32 SlotColumn = Index % MaxSlotColumn;
+        int32 SlotRow = Index / MaxSlotColumn;
+        QuickSlotPanel->AddChildToUniformGrid(SlotWidget, SlotRow, SlotColumn);
+
+        SlotWidgetMap.Emplace(Index, SlotWidget);
+    }
+}
---
+++ Source/UniversalGameFramework/Public/Widgets/QuickSlot/UGFQuickSlotPanelWidget.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/QuickSlotManagerWidgetBase.h"
+#include "UGFQuickSlotPanelWidget.generated.h"
+
+class UUGFQuickSlotWidget;
+class UUniformGridPanel;
+
+/**
+ *
+ */
+UCLASS()
+class UNIVERSALGAMEFRAMEWORK_API UUGFQuickSlotPanelWidget : public UQuickSlotManagerWidgetBase
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UUniformGridPanel> QuickSlotPanel;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSubclassOf<UUGFQuickSlotWidget> SlotWidgetClass;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Config", meta = (ClampMin = 1))
+    int32 MaxSlotColumn = 4;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    int32 PreviewSlotNum = 4;
+
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "State")
+    TMap<int32, TObjectPtr<UUGFQuickSlotWidget>> SlotWidgetMap;
+
+protected:
+    virtual void NativePreConstruct() override;
+
+    virtual void CreateSlotWidgets();
+};
