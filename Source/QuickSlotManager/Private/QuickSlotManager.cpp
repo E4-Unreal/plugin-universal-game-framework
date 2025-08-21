0dcd7e5b17f01d9ed851a6d827b4ec3e2ccc8d30
이정훈 <eu4ng97@gmail.com>
2025-08-21 11:18:39 +0900

    Chore: QuickSlotManager 모듈 생성

---
+++ Source/QuickSlotManager/Private/QuickSlotManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "QuickSlotManager.h"
+
+#define LOCTEXT_NAMESPACE "FQuickSlotManagerModule"
+
+void FQuickSlotManagerModule::StartupModule()
+{
+    
+}
+
+void FQuickSlotManagerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FQuickSlotManagerModule, QuickSlotManager)
---
+++ Source/QuickSlotManager/QuickSlotManager.Build.cs
@@ -0,0 +1,30 @@
+﻿using UnrealBuildTool;
+
+public class QuickSlotManager : ModuleRules
+{
+    public QuickSlotManager(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "GameplayTags",
+
+                "UniversalUtilities",
+                "SocketManager"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
