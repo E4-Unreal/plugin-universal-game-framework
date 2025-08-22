8f00b604db570f05366a97392754d26bed9f9c49
이정훈 <eu4ng97@gmail.com>
2025-08-23 05:13:50 +0900

    Feat: QuickSlotWidgetInterface, QuickSlotManagerInterface 정의

---
+++ Source/QuickSlotManager/Private/Interfaces/QuickSlotManagerInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/QuickSlotManagerInterface.h"
+
+
+// Add default functionality here for any IQuickSlotManagerInterface functions that are not pure virtual.
---
+++ Source/QuickSlotManager/Public/Interfaces/QuickSlotManagerInterface.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "QuickSlotManagerInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class UQuickSlotManagerInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class QUICKSLOTMANAGER_API IQuickSlotManagerInterface
+{
+    GENERATED_BODY()
+
+public:
+};
