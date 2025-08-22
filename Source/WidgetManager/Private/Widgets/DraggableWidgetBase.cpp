12c26ead120fe05e3043ad9e239f65f506e4b1cc
이정훈 <eu4ng97@gmail.com>
2025-08-23 05:06:15 +0900

    Feat: DraggableWidgetBase 클래스 작성

---
+++ Source/WidgetManager/Private/Widgets/DraggableWidgetBase.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/DraggableWidgetBase.h"
+
+#include "Blueprint/WidgetBlueprintLibrary.h"
+
+FReply UDraggableWidgetBase::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+    if (InMouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton) && CanDrag())
+    {
+        FEventReply EventReply = UWidgetBlueprintLibrary::DetectDragIfPressed(InMouseEvent, this, EKeys::LeftMouseButton);
+        return EventReply.NativeReply;
+    }
+
+    return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+}
+
+void UDraggableWidgetBase::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent,
+    UDragDropOperation*& OutOperation)
+{
+    if (UUserWidget* DraggedWidget = CreateDraggedWidget())
+    {
+        UDragDropOperation* DragAndDropOperation = NewObject<UDragDropOperation>(this, UDragDropOperation::StaticClass());
+        DragAndDropOperation->DefaultDragVisual = DraggedWidget;
+
+        OutOperation = DragAndDropOperation;
+    }
+    else
+    {
+        Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
+    }
+}
+
+bool UDraggableWidgetBase::CanDrag() const
+{
+    return true;
+}
+
+UUserWidget* UDraggableWidgetBase::CreateDraggedWidget()
+{
+    UUserWidget* DraggedWidget = nullptr;
+
+    if (DraggedWidgetClass)
+    {
+        DraggedWidget = CreateWidget<UUserWidget>(GetOwningPlayer(), DraggedWidgetClass);
+        OnDraggedWidgetCreated(DraggedWidget);
+    }
+
+    return DraggedWidget;
+}
+
+void UDraggableWidgetBase::OnDraggedWidgetCreated(UUserWidget* DraggedWidget)
+{
+    // DraggedWidget 초기화 및 설정
+}
