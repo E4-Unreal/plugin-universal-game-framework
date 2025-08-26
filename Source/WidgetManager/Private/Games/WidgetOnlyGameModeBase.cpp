1aa7a09a2f1ea43e9a7ff411a5bdba9ee591ea91
이정훈 <eu4ng97@gmail.com>
2025-08-27 00:28:48 +0900

    Feat: WidgetOnlyGameModeBase 클래스 추가

---
+++ Source/WidgetManager/Private/Games/WidgetOnlyGameModeBase.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Games/WidgetOnlyGameModeBase.h"
+
+#include "Blueprint/UserWidget.h"
+
+AWidgetOnlyGameModeBase::AWidgetOnlyGameModeBase(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    DefaultPawnClass = nullptr;
+    SpectatorClass = nullptr;
+}
+
+void AWidgetOnlyGameModeBase::Destroyed()
+{
+    DestroyWidgets();
+
+    Super::Destroyed();
+}
+
+void AWidgetOnlyGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    CreateWidgets();
+    ShowWidgets();
+    SetPlayerUIMode();
+}
+
+void AWidgetOnlyGameModeBase::CreateWidgets()
+{
+    Widgets.Reset(WidgetClasses.Num());
+    for (TSubclassOf<UUserWidget> WidgetClass : WidgetClasses)
+    {
+        if (WidgetClass == nullptr) continue;
+
+        UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), WidgetClass);
+        Widgets.Emplace(Widget);
+    }
+}
+
+void AWidgetOnlyGameModeBase::DestroyWidgets()
+{
+    for (UUserWidget* Widget : Widgets)
+    {
+        if (Widget->IsInViewport())
+        {
+            Widget->RemoveFromParent();
+        }
+    }
+    Widgets.Empty();
+}
+
+void AWidgetOnlyGameModeBase::ShowWidgets()
+{
+    for (UUserWidget* Widget : Widgets)
+    {
+        if (!Widget->IsInViewport())
+        {
+            Widget->AddToViewport();
+        }
+
+        if (!Widget->IsVisible())
+        {
+            Widget->SetVisibility(ESlateVisibility::Visible);
+        }
+    }
+}
+
+void AWidgetOnlyGameModeBase::HideWidgets()
+{
+    for (UUserWidget* Widget : Widgets)
+    {
+        if (Widget->IsVisible())
+        {
+            Widget->SetVisibility(ESlateVisibility::Collapsed);
+        }
+    }
+}
+
+void AWidgetOnlyGameModeBase::SetPlayerUIMode()
+{
+    if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
+    {
+        PlayerController->SetShowMouseCursor(true);
+        PlayerController->SetIgnoreMoveInput(true);
+        PlayerController->SetIgnoreLookInput(true);
+    }
+}
+
---
+++ Source/WidgetManager/Public/Games/WidgetOnlyGameModeBase.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WidgetOnlyGameModeBase.generated.h"
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class WIDGETMANAGER_API AWidgetOnlyGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TArray<TSubclassOf<UUserWidget>> WidgetClasses;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TArray<TObjectPtr<UUserWidget>> Widgets;
+
+public:
+    virtual void Destroyed() override;
+
+protected:
+    AWidgetOnlyGameModeBase(const FObjectInitializer& ObjectInitializer);
+
+    virtual void BeginPlay() override;
+
+    virtual void CreateWidgets();
+    virtual void DestroyWidgets();
+    virtual void ShowWidgets();
+    virtual void HideWidgets();
+
+    virtual void SetPlayerUIMode();
+};
