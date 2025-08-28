09d3d17e2192fa6269dcdb0a82d898ad5450953c
이정훈 <eu4ng97@gmail.com>
2025-08-29 00:35:05 +0900

    Chore: CommonWidgetManagerEditor 모듈 추가

---
+++ Source/CommonWidgetManagerEditor/Private/CommonWidgetManagerEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "CommonWidgetManagerEditor.h"
+
+#define LOCTEXT_NAMESPACE "FCommonWidgetManagerEditorModule"
+
+void FCommonWidgetManagerEditorModule::StartupModule()
+{
+    
+}
+
+void FCommonWidgetManagerEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCommonWidgetManagerEditorModule, CommonWidgetManagerEditor)
