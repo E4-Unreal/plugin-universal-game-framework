f02ba0015eca1382778d6699bf0f7db1fc0bb55f
이정훈 <eu4ng97@gmail.com>
2025-08-30 00:15:21 +0900

    Feat: CommonButtonStyleAssetAction 클래스 추가

---
+++ Source/CommonWidgetManagerEditor/Private/AssetActions/CommonButtonStyleAssetAction.cpp
@@ -0,0 +1,102 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetActions/CommonButtonStyleAssetAction.h"
+
+#include "CommonButtonBase.h"
+
+UCommonButtonStyleAssetAction::UCommonButtonStyleAssetAction()
+{
+    SupportedClasses.Emplace(UCommonButtonStyle::StaticClass());
+
+    HalfHeightRadiusName = "HHR";
+
+    RadiusMap.Emplace("R0", FVector4::Zero());
+    RadiusMap.Emplace("R1", FVector4(10, 10, 10, 10));
+    RadiusMap.Emplace("R2", FVector4(15, 15, 15, 15));
+    RadiusMap.Emplace("R3", FVector4(20, 20, 20, 20));
+}
+
+
+bool UCommonButtonStyleAssetAction::SetHalfHeightRadius(FSlateBrush& SlateBrush)
+{
+    bool bDirty = false;
+
+    if (SlateBrush.DrawAs != ESlateBrushDrawType::RoundedBox)
+    {
+        SlateBrush.DrawAs = ESlateBrushDrawType::RoundedBox;
+        bDirty = true;
+    }
+
+    if (SlateBrush.OutlineSettings.RoundingType != ESlateBrushRoundingType::HalfHeightRadius)
+    {
+        SlateBrush.OutlineSettings.RoundingType = ESlateBrushRoundingType::HalfHeightRadius;
+        bDirty = true;
+    }
+
+    return bDirty;
+}
+
+bool UCommonButtonStyleAssetAction::SetRadius(FSlateBrush& SlateBrush, const FVector4& Radius)
+{
+    bool bDirty = false;
+
+    if (SlateBrush.OutlineSettings.CornerRadii != Radius)
+    {
+        SlateBrush.DrawAs = Radius == FVector4::Zero() ? ESlateBrushDrawType::Box : ESlateBrushDrawType::RoundedBox;
+        SlateBrush.OutlineSettings.CornerRadii = Radius;
+
+        bDirty = true;
+    }
+
+    if (Radius == FVector4::Zero() && SlateBrush.DrawAs != ESlateBrushDrawType::Box)
+    {
+        SlateBrush.DrawAs = ESlateBrushDrawType::Box;
+
+        bDirty = true;
+    }
+    else if (Radius != FVector4::Zero() && SlateBrush.DrawAs != ESlateBrushDrawType::RoundedBox)
+    {
+        SlateBrush.DrawAs = ESlateBrushDrawType::RoundedBox;
+
+        bDirty = true;
+    }
+
+    return bDirty;
+}
+
+void UCommonButtonStyleAssetAction::OnGenerateInstance(const FAssetData& SelectedAsset)
+{
+    UObject* StyleInstance = GetOrCreateStyleInstance(SelectedAsset, HalfHeightRadiusName.ToString());
+    if (UCommonButtonStyle* Style = Cast<UCommonButtonStyle>(StyleInstance))
+    {
+        bool bDirty = false;
+        bDirty = SetHalfHeightRadius(Style->NormalBase) || bDirty;
+        bDirty = SetHalfHeightRadius(Style->NormalHovered) || bDirty;
+        bDirty = SetHalfHeightRadius(Style->NormalPressed) || bDirty;
+        bDirty = SetHalfHeightRadius(Style->SelectedBase) || bDirty;
+        bDirty = SetHalfHeightRadius(Style->SelectedHovered) || bDirty;
+        bDirty = SetHalfHeightRadius(Style->SelectedPressed) || bDirty;
+        bDirty = SetHalfHeightRadius(Style->Disabled) || bDirty;
+
+        if (bDirty) Style->GetPackage()->MarkPackageDirty();
+    }
+
+    for (const auto& [RadiusName, RadiusSize] : RadiusMap)
+    {
+        StyleInstance = GetOrCreateStyleInstance(SelectedAsset, RadiusName.ToString());
+        if (UCommonButtonStyle* Style = Cast<UCommonButtonStyle>(StyleInstance))
+        {
+            bool bDirty = false;
+            bDirty = SetRadius(Style->NormalBase, RadiusSize) || bDirty;
+            bDirty = SetRadius(Style->NormalHovered, RadiusSize) || bDirty;
+            bDirty = SetRadius(Style->NormalPressed, RadiusSize) || bDirty;
+            bDirty = SetRadius(Style->SelectedBase, RadiusSize) || bDirty;
+            bDirty = SetRadius(Style->SelectedHovered, RadiusSize) || bDirty;
+            bDirty = SetRadius(Style->SelectedPressed, RadiusSize) || bDirty;
+            bDirty = SetRadius(Style->Disabled, RadiusSize) || bDirty;
+
+            if (bDirty) Style->GetPackage()->MarkPackageDirty();
+        }
+    }
+}
---
+++ Source/CommonWidgetManagerEditor/Public/AssetActions/CommonButtonStyleAssetAction.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonStyleAssetActionBase.h"
+#include "CommonButtonStyleAssetAction.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class COMMONWIDGETMANAGEREDITOR_API UCommonButtonStyleAssetAction : public UCommonStyleAssetActionBase
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    FName HalfHeightRadiusName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TMap<FName, FVector4> RadiusMap;
+
+public:
+    UCommonButtonStyleAssetAction();
+
+protected:
+    static bool SetHalfHeightRadius(FSlateBrush& SlateBrush);
+    static bool SetRadius(FSlateBrush& SlateBrush, const FVector4& Radius);
+
+    virtual void OnGenerateInstance(const FAssetData& SelectedAsset) override;
+};
