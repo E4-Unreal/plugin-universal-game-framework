709f098fc58663834c2cce2267034bdd1d709945
이정훈 <eu4ng97@gmail.com>
2025-08-30 11:26:05 +0900

    Feat: CommonTextActionButtonBase 클래스 추가

---
+++ Source/CommonWidgetManager/Private/Widgets/CommonTextActionButtonBase.cpp
@@ -0,0 +1,112 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/CommonTextActionButtonBase.h"
+
+#include "CommonBorder.h"
+#include "CommonTextBlock.h"
+
+void UCommonTextActionButtonBase::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+    SetActionButtonNormal();
+}
+
+void UCommonTextActionButtonBase::NativeOnHovered()
+{
+    Super::NativeOnHovered();
+
+    SetActionButtonHovered();
+}
+
+void UCommonTextActionButtonBase::NativeOnUnhovered()
+{
+    Super::NativeOnUnhovered();
+
+    SetActionButtonNormal();
+}
+
+void UCommonTextActionButtonBase::NativeOnPressed()
+{
+    Super::NativeOnPressed();
+
+    SetActionButtonPressed();
+}
+
+void UCommonTextActionButtonBase::NativeOnReleased()
+{
+    Super::NativeOnReleased();
+
+    SetActionButtonNormal();
+}
+
+void UCommonTextActionButtonBase::SetActionButtonNormal()
+{
+    if (ActionButtonStyle)
+    {
+        UCommonButtonStyle* ButtonStyle = Cast<UCommonButtonStyle>(ActionButtonStyle->ClassDefaultObject);
+        if (GetSelected())
+        {
+            SetActionBorderBrush(ButtonStyle->SelectedBase);
+            SetActionTextStyle(ButtonStyle->SelectedTextStyle);
+        }
+        else
+        {
+            SetActionBorderBrush(ButtonStyle->NormalBase);
+            SetActionTextStyle(ButtonStyle->NormalTextStyle);
+        }
+    }
+}
+
+void UCommonTextActionButtonBase::SetActionButtonHovered()
+{
+    if (ActionButtonStyle)
+    {
+        UCommonButtonStyle* ButtonStyle = Cast<UCommonButtonStyle>(ActionButtonStyle->ClassDefaultObject);
+        if (GetSelected())
+        {
+            SetActionBorderBrush(ButtonStyle->SelectedHovered);
+            SetActionTextStyle(ButtonStyle->SelectedHoveredTextStyle);
+        }
+        else
+        {
+            SetActionBorderBrush(ButtonStyle->NormalHovered);
+            SetActionTextStyle(ButtonStyle->NormalHoveredTextStyle);
+        }
+    }
+}
+
+void UCommonTextActionButtonBase::SetActionButtonPressed()
+{
+    if (ActionButtonStyle)
+    {
+        UCommonButtonStyle* ButtonStyle = Cast<UCommonButtonStyle>(ActionButtonStyle->ClassDefaultObject);
+        if (GetSelected())
+        {
+            SetActionBorderBrush(ButtonStyle->SelectedPressed);
+            SetActionTextStyle(ButtonStyle->SelectedTextStyle);
+        }
+        else
+        {
+            SetActionBorderBrush(ButtonStyle->NormalPressed);
+            SetActionTextStyle(ButtonStyle->NormalTextStyle);
+        }
+    }
+}
+
+void UCommonTextActionButtonBase::SetActionBorderBrush(const FSlateBrush& NewBrush)
+{
+    if (GetActionBorder())
+    {
+        GetActionBorder()->SetBrush(NewBrush);
+    }
+}
+
+void UCommonTextActionButtonBase::SetActionTextStyle(TSubclassOf<UCommonTextStyle> NewStyle)
+{
+    if (GetActionTextBlock())
+    {
+        GetActionTextBlock()->SetStyle(NewStyle);
+    }
+}
---
+++ Source/CommonWidgetManager/Public/Widgets/CommonTextActionButtonBase.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTextButtonBase.h"
+#include "CommonTextActionButtonBase.generated.h"
+
+class UBorder;
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class COMMONWIDGETMANAGER_API UCommonTextActionButtonBase : public UCommonTextButtonBase
+{
+    GENERATED_BODY()
+
+private:
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UBorder> ActionBorder;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> ActionTextBlock;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSubclassOf<UCommonButtonStyle> ActionButtonStyle;
+
+public:
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UBorder* GetActionBorder() const { return ActionBorder; }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UCommonTextBlock* GetActionTextBlock() const { return ActionTextBlock; }
+
+protected:
+    /* UserWidget */
+
+    virtual void NativePreConstruct() override;
+
+    /* CommonButtonBase */
+
+    virtual void NativeOnHovered() override;
+    virtual void NativeOnUnhovered() override;
+    virtual void NativeOnPressed() override;
+    virtual void NativeOnReleased() override;
+
+    /* API */
+
+    virtual void SetActionButtonNormal();
+    virtual void SetActionButtonHovered();
+    virtual void SetActionButtonPressed();
+
+    virtual void SetActionBorderBrush(const FSlateBrush& NewBrush);
+    virtual void SetActionTextStyle(TSubclassOf<UCommonTextStyle> NewStyle);
+};
