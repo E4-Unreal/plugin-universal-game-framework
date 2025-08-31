b13fbe324455fd102e5fc9c01b07c6a86fd775d0
이정훈 <eu4ng97@gmail.com>
2025-08-31 22:34:46 +0900

    Chore: DataManager 모듈 추가

---
+++ Source/DataManager/DataManager.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class DataManager : ModuleRules
+{
+    public DataManager(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
