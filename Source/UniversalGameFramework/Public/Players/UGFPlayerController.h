64a1db42daad3c7f35133702ef1d7ebdafacd42e
이정훈 <eu4ng97@gmail.com>
2025-09-01 23:18:01 +0900

    Feat: UGFPlayerController 클래스 추가

---
+++ Source/UniversalGameFramework/Public/Players/UGFPlayerController.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UGFPlayerController.generated.h"
+
+class UCommonWidgetManagerComponent;
+
+/**
+ *
+ */
+UCLASS()
+class UNIVERSALGAMEFRAMEWORK_API AUGFPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+protected:
+    const static FName WidgetManagerName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UCommonWidgetManagerComponent> WidgetManager;
+
+public:
+    AUGFPlayerController(const FObjectInitializer& ObjectInitializer);
+
+    FORCEINLINE UCommonWidgetManagerComponent* GetWidgetManager() const { return WidgetManager; }
+};
