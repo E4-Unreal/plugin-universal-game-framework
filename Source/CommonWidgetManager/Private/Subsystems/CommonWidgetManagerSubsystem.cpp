cc82cd7c247c8c6c4c8fa81b52db4365854214b4
이정훈 <eu4ng97@gmail.com>
2025-09-01 18:56:48 +0900

    Feat: CommonWidetManagerSubsystem 클래스 추가

---
+++ Source/CommonWidgetManager/Private/Subsystems/CommonWidgetManagerSubsystem.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystems/CommonWidgetManagerSubsystem.h"
+
+#include "Components/CommonWidgetManagerComponent.h"
+#include "Widgets/CommonLayoutWidgetBase.h"
+
+bool UCommonWidgetManagerSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+    TArray<UClass*> ChildClasses;
+    GetDerivedClasses(GetClass(), ChildClasses, false);
+
+    return ChildClasses.Num() == 0;
+}
+
+void UCommonWidgetManagerSubsystem::ShowAlertWidget(APlayerController* PlayerController, const FText& TitleText,
+    const FText& MessageText)
+{
+    if (PlayerController && PlayerController->IsLocalController())
+    {
+        if (UCommonWidgetManagerComponent* CommonWidgetManager = PlayerController->GetComponentByClass<UCommonWidgetManagerComponent>())
+        {
+            CommonWidgetManager->ShowAlertWidget(TitleText, MessageText);
+        }
+    }
+}
+
+void UCommonWidgetManagerSubsystem::ShowConfirmWidget(APlayerController* PlayerController, const FText& TitleText,
+    const FText& MessageText, const FButtonClickedDelegate& ConfirmButtonClickedDelegate)
+{
+    if (PlayerController && PlayerController->IsLocalController())
+    {
+        if (UCommonWidgetManagerComponent* CommonWidgetManager = PlayerController->GetComponentByClass<UCommonWidgetManagerComponent>())
+        {
+            CommonWidgetManager->ShowConfirmWidget(TitleText, MessageText, ConfirmButtonClickedDelegate);
+        }
+    }
+}
---
+++ Source/CommonWidgetManager/Public/Subsystems/CommonWidgetManagerSubsystem.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CommonWidgetManagerSubsystem.generated.h"
+
+class FButtonClickedDelegate;
+
+/**
+ *
+ */
+UCLASS()
+class COMMONWIDGETMANAGER_API UCommonWidgetManagerSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ShowAlertWidget(APlayerController* PlayerController, const FText& TitleText, const FText& MessageText);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ShowConfirmWidget(APlayerController* PlayerController, const FText& TitleText, const FText& MessageText, const FButtonClickedDelegate& ConfirmButtonClickedDelegate);
+};
