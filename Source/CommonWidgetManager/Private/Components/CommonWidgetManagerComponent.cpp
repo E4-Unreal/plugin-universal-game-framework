0f64debd8d77d28e4fee370479b05eae8f9ede0a
이정훈 <eu4ng97@gmail.com>
2025-09-01 17:52:27 +0900

    Feat: CommonWidgetManagerComponent 클래스 추가

---
+++ Source/CommonWidgetManager/Private/Components/CommonWidgetManagerComponent.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/CommonWidgetManagerComponent.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Widgets/CommonLayoutWidgetBase.h"
+
+void UCommonWidgetManagerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (APlayerController* OwningPlayerController = GetOwningPlayerController())
+    {
+        if (LayoutWidgetClass)
+        {
+            LayoutWidget = CreateWidget<UCommonLayoutWidgetBase>(OwningPlayerController, LayoutWidgetClass);
+            LayoutWidget->AddToViewport();
+        }
+    }
+}
+
+void UCommonWidgetManagerComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+    if (LayoutWidget)
+    {
+        LayoutWidget->RemoveFromParent();
+    }
+
+    Super::OnComponentDestroyed(bDestroyingHierarchy);
+}
+
+APlayerController* UCommonWidgetManagerComponent::GetOwningPlayerController() const
+{
+    APlayerController* OwningPlayerController = nullptr;
+    if (AActor* OwningActor = GetOwner())
+    {
+        if (OwningActor->IsA(APlayerController::StaticClass()))
+        {
+            OwningPlayerController = Cast<APlayerController>(OwningActor);
+        }
+        else if (OwningActor->IsA(APawn::StaticClass()))
+        {
+            APawn* OwningPawn = Cast<APawn>(OwningActor);
+            OwningPlayerController = Cast<APlayerController>(OwningPawn->GetController());
+        }
+
+        if (OwningPlayerController && !OwningPlayerController->IsLocalController())
+        {
+            OwningPlayerController = nullptr;
+        }
+    }
+
+    return OwningPlayerController;
+}
---
+++ Source/CommonWidgetManager/Public/Components/CommonWidgetManagerComponent.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CommonWidgetManagerComponent.generated.h"
+
+
+class UCommonLayoutWidgetBase;
+class UCommonActivatableWidget;
+
+/**
+ * PlayerController 전용 액터 컴포넌트
+ */
+UCLASS(meta=(BlueprintSpawnableComponent))
+class COMMONWIDGETMANAGER_API UCommonWidgetManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSubclassOf<UCommonLayoutWidgetBase> LayoutWidgetClass;
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TObjectPtr<UCommonLayoutWidgetBase> LayoutWidget;
+
+public:
+    virtual void BeginPlay() override;
+    virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
+    UFUNCTION(BlueprintPure)
+    virtual APlayerController* GetOwningPlayerController() const;
+
+    UFUNCTION(BlueprintPure)
+    UCommonLayoutWidgetBase* GetLayoutWidget() const { return LayoutWidget; }
+};
