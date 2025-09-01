be84719c3f3fd3cf16d03f273047692cd3b500fc
이정훈 <eu4ng97@gmail.com>
2025-09-01 18:27:13 +0900

    Feat: CommonPopupWidgetBase 클래스 추가

---
+++ Source/CommonWidgetManager/Private/Widgets/CommonPopupWidgetBase.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/CommonPopupWidgetBase.h"
+
+#include "CommonButtonBase.h"
+#include "CommonTextBlock.h"
+
+void UCommonPopupWidgetBase::SetTitleText_Implementation(const FText& TitleText)
+{
+    if (GetTitleTextBlock())
+    {
+        GetTitleTextBlock()->SetText(TitleText);
+    }
+}
+
+void UCommonPopupWidgetBase::SetMessageText_Implementation(const FText& MessageText)
+{
+    if (GetMessageTextBlock())
+    {
+        GetMessageTextBlock()->SetText(MessageText);
+    }
+}
+
+void UCommonPopupWidgetBase::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+
+    if (GetConfirmButton())
+    {
+        GetConfirmButton()->OnClicked().AddUObject(this, &ThisClass::OnConfirmButtonClicked);
+    }
+
+    if (GetCancelButton())
+    {
+        GetCancelButton()->OnClicked().AddUObject(this, &ThisClass::OnCancelButtonClicked);
+    }
+}
+
+void UCommonPopupWidgetBase::OnConfirmButtonClicked_Implementation()
+{
+    if (ConfirmButtonClickedDelegate.IsBound())
+    {
+        ConfirmButtonClickedDelegate.Execute();
+        ConfirmButtonClickedDelegate.Clear();
+    }
+
+    ConfirmButtonClickedEvent.Broadcast();
+
+    DeactivateWidget();
+}
+
+void UCommonPopupWidgetBase::OnCancelButtonClicked_Implementation()
+{
+    if (CancelButtonClickedDelegate.IsBound())
+    {
+        CancelButtonClickedDelegate.Execute();
+        CancelButtonClickedDelegate.Clear();
+    }
+
+    CancelButtonClickedEvent.Broadcast();
+
+    DeactivateWidget();
+}
---
+++ Source/CommonWidgetManager/Public/Widgets/CommonPopupWidgetBase.h
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "Components/Button.h"
+#include "CommonPopupWidgetBase.generated.h"
+
+class UCommonTextBlock;
+class UCommonButtonBase;
+
+DECLARE_DYNAMIC_DELEGATE(FButtonClickedDelegate);
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class COMMONWIDGETMANAGER_API UCommonPopupWidgetBase : public UCommonActivatableWidget
+{
+    GENERATED_BODY()
+
+private:
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UCommonTextBlock> TitleTextBlock;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> MessageTextBlock;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonButtonBase> ConfirmButton;
+
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UCommonButtonBase> CancelButton;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnButtonClickedEvent ConfirmButtonClickedEvent;
+
+    FButtonClickedDelegate ConfirmButtonClickedDelegate;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnButtonClickedEvent CancelButtonClickedEvent;
+
+    FButtonClickedDelegate CancelButtonClickedDelegate;
+
+    FSimpleDelegate OnConfirmButtonClickedDelegate;
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetTitleText(const FText& TitleText);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetMessageText(const FText& MessageText);
+
+    UFUNCTION(BlueprintCallable)
+    FORCEINLINE void SetConfirmButtonClickedDelegate(const FButtonClickedDelegate& NewDelegate) { ConfirmButtonClickedDelegate = NewDelegate; }
+
+    UFUNCTION(BlueprintCallable)
+    FORCEINLINE void SetCancelButtonClickedDelegate(const FButtonClickedDelegate& NewDelegate) { CancelButtonClickedDelegate = NewDelegate; }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UCommonTextBlock* GetTitleTextBlock() const { return TitleTextBlock; }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UCommonTextBlock* GetMessageTextBlock() const { return MessageTextBlock; }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UCommonButtonBase* GetConfirmButton() const { return ConfirmButton; }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UCommonButtonBase* GetCancelButton() const { return CancelButton; }
+
+protected:
+    virtual void NativeOnInitialized() override;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void OnConfirmButtonClicked();
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void OnCancelButtonClicked();
+};
