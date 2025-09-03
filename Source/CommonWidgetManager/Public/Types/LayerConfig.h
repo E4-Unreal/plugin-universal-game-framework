e2f18499da9bfb4e903a67e5857eb2ada3d8fc79
이정훈 <eu4ng97@gmail.com>
2025-09-03 14:45:33 +0900

    Feat: LayerConfig 구조체 추가

---
+++ Source/CommonWidgetManager/Public/Types/LayerConfig.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LayerConfig.generated.h"
+
+enum class ECommonSwitcherTransition : uint8;
+enum class ETransitionCurve : uint8;
+enum class ECommonSwitcherTransitionFallbackStrategy : uint8;
+
+USTRUCT(BlueprintType)
+struct COMMONWIDGETMANAGER_API FLayerConfig
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    bool bIsOverlay;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    ECommonSwitcherTransition TransitionType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    ETransitionCurve TransitionCurveType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float TransitionDuration = 0.4f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    ECommonSwitcherTransitionFallbackStrategy TransitionFallbackStrategy;
+};
