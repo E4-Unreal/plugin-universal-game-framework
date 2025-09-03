83bdcc19d9c78cc6f4ba3b46e530761ef27e6652
이정훈 <eu4ng97@gmail.com>
2025-09-04 05:53:37 +0900

    Feat: WeaponGameplayTags 추가

---
+++ Source/WeaponManager/Public/GameplayTags/WeaponGameplayTags.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WeaponManager
+{
+    namespace Weapon
+    {
+        WEAPONMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+
+        namespace Slot
+        {
+            WEAPONMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Root)
+
+            WEAPONMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary)
+            WEAPONMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Secondary)
+            WEAPONMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Special)
+        }
+    }
+}
+
+using namespace WeaponManager;
