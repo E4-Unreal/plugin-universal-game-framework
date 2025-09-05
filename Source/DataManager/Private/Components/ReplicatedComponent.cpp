378c189ee5ffd6dec3afcebf79e8586aa4a28160
이정훈 <eu4ng97@gmail.com>
2025-09-05 14:45:55 +0900

    Feat: ReplicatedObject, ReplicatedComponent 클래스 추가

---
+++ Source/DataManager/Private/Components/ReplicatedComponent.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/ReplicatedComponent.h"
+
+UReplicatedComponent::UReplicatedComponent()
+{
+    SetIsReplicatedByDefault(true);
+    bReplicateUsingRegisteredSubObjectList = true;
+}
+
+UReplicatedObject* UReplicatedComponent::CreateReplicatedObject(TSubclassOf<UReplicatedObject> ReplicatedObjectClass)
+{
+    UReplicatedObject* NewReplicatedObject = nullptr;
+
+    if (GetOwner()->HasAuthority() && ReplicatedObjectClass)
+    {
+        NewReplicatedObject = NewObject<UReplicatedObject>(this, ReplicatedObjectClass);
+        ReplicatedObjects.Emplace(NewReplicatedObject);
+
+        AddReplicatedSubObject(NewReplicatedObject);
+    }
+
+    return NewReplicatedObject;
+}
+
+void UReplicatedComponent::DestroyReplicatedObject(UReplicatedObject* ReplicatedObject)
+{
+    if (GetOwner()->HasAuthority() && ReplicatedObject)
+    {
+        RemoveReplicatedSubObject(ReplicatedObject);
+
+        ReplicatedObjects.RemoveSingleSwap(ReplicatedObject);
+    }
+}
---
+++ Source/DataManager/Public/Components/ReplicatedComponent.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Data/ReplicatedObject.h"
+#include "ReplicatedComponent.generated.h"
+
+
+class UReplicatedObject;
+
+UCLASS(meta=(BlueprintSpawnableComponent))
+class DATAMANAGER_API UReplicatedComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
+    TArray<TObjectPtr<UReplicatedObject>> ReplicatedObjects;
+
+public:
+    UReplicatedComponent();
+
+protected:
+    virtual UReplicatedObject* CreateReplicatedObject(TSubclassOf<UReplicatedObject> ReplicatedObjectClass);
+    virtual void DestroyReplicatedObject(UReplicatedObject* ReplicatedObject);
+
+    template <typename TReplicatedObject = UReplicatedObject>
+    TReplicatedObject* CreateReplicatedObject(TSubclassOf<UReplicatedObject> ReplicatedObjectClass)
+    {
+        if (ReplicatedObjectClass && ReplicatedObjectClass->IsChildOf<TReplicatedObject>())
+        {
+            return Cast<TReplicatedObject>(CreateReplicatedObject(ReplicatedObjectClass));
+        }
+
+        return nullptr;
+    }
+};
