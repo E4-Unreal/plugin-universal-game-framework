d0f7608d7be84bb86adec42fcad4c39ac7a24361
이정훈 <eu4ng97@gmail.com>
2025-09-05 23:11:20 +0900

    Feat: CommonToggleWidgetButtonBase 클래스 추가

---
+++ Source/CommonWidgetManager/Public/Widgets/Button/CommonToggleWidgetButtonBase.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "CommonToggleWidgetButtonBase.generated.h"
+
+class UCommonLayerWidgetBase;
+
+/**
+ *
+ */
+UCLASS()
+class COMMONWIDGETMANAGER_API UCommonToggleWidgetButtonBase : public UCommonButtonBase
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSubclassOf<UCommonLayerWidgetBase> LayerWidgetClass;
+
+protected:
+    virtual void NativeOnClicked() override;
+};
