bce1f4bf0f95ed45f5f238f7b9f968eef5f9ef48
이정훈 <eu4ng97@gmail.com>
2025-09-06 00:57:57 +0900

    Feat: SlotManager 모듈 추가

---
+++ Source/SlotManager/Private/SlotManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "SlotManager.h"
+
+#define LOCTEXT_NAMESPACE "FSlotManagerModule"
+
+void FSlotManagerModule::StartupModule()
+{
+    
+}
+
+void FSlotManagerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSlotManagerModule, SlotManager)
---
+++ Source/SlotManager/SlotManager.Build.cs
@@ -0,0 +1,28 @@
+﻿using UnrealBuildTool;
+
+public class SlotManager : ModuleRules
+{
+    public SlotManager(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities",
+                "DataManager"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
