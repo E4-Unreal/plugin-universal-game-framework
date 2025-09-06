a14d818e9e00f964f5152d775c9f017f4b1f4217
이정훈 <eu4ng97@gmail.com>
2025-09-06 10:20:53 +0900

    Feat: SlotContent 클래스 추가

---
+++ Source/SlotManager/Public/Objects/SlotContent.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/ReplicatedObject.h"
+#include "SlotContent.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SLOTMANAGER_API USlotContent : public UReplicatedObject
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TObjectPtr<UDataAsset> Data;
+};
