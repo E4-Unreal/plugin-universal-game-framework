aef84ab008325008fd1acf5f4155c0886ef432c3
이정훈 <eu4ng97@gmail.com>
2025-09-08 00:30:34 +0900

    Feat: ItemInstanceInterface 인터페이스 추가

--- Source/InventorySystem/InventorySystem.Build.cs
+++ Source/InventorySystem/InventorySystem.Build.cs
@@ -15,7 +15,8 @@ public class InventorySystem : ModuleRules
                 "CommonUI",
                 "InputCore",
                 "UniversalUtilities",
-                "InteractionSystem"
+                "InteractionSystem",
+                "SlotManager",
             }
         );
 
---
+++ Source/InventorySystem/Public/Interfaces/ItemInstanceInterface.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ItemInstanceInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class UItemInstanceInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class INVENTORYSYSTEM_API IItemInstanceInterface
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    UDataAsset* GetData() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetData(UDataAsset* NewData);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    int32 GetQuantity() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetQuantity(int32 NewQuantity);
+};
