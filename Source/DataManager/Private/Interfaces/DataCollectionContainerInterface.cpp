66d86ff56b9b9c6813726b4e6f01dcb9045b18e0
이정훈 <eu4ng97@gmail.com>
2025-09-08 23:08:37 +0900

    Feat: DataCollectionContainerInterface 인터페이스 추가

---
+++ Source/DataManager/Private/Interfaces/DataCollectionContainerInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/DataCollectionContainerInterface.h"
+
+
+// Add default functionality here for any IDataCollectionContainerInterface functions that are not pure virtual.
