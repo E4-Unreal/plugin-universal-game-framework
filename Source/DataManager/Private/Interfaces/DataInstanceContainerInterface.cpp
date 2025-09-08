8f7be73f1b089c4b3f47c36d3f9ff4014016ab0a
이정훈 <eu4ng97@gmail.com>
2025-09-08 22:41:07 +0900

    Feat: DataInstanceContainerInterface 인터페이스 추가

---
+++ Source/DataManager/Private/Interfaces/DataInstanceContainerInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/DataInstanceContainerInterface.h"
+
+
+// Add default functionality here for any IDataInstanceContainerInterface functions that are not pure virtual.
