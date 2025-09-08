fe3026c6c676a0ea436c55ccf394654b2d71c2ec
이정훈 <eu4ng97@gmail.com>
2025-09-08 12:53:43 +0900

    Feat: DataInstance 클래스 추가

---
+++ Source/DataManager/Private/Data/DataInstance.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/DataInstance.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UDataInstance::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(ThisClass, Data);
+}
+
+UDataAsset* UDataInstance::GetData_Implementation() const
+{
+    return Data;
+}
+
+void UDataInstance::SetData_Implementation(UDataAsset* NewData)
+{
+    Data = NewData;
+}
