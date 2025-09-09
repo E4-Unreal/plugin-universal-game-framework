471e2ee0c8cb598c3b8cd0d0a2ac413ffe188d8e
이정훈 <eu4ng97@gmail.com>
2025-09-09 20:09:00 +0900

    Feat: UGFEquipmentDefinition 클래스 추가

---
+++ Source/UniversalGameFramework/Private/Data/UGFEquipmentDefinition.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/UGFEquipmentDefinition.h"
+
+#include "GameplayTags/WeaponGameplayTags.h"
+#include "Actors/WeaponActor.h"
+#include "Data/WeaponInstance.h"
+
+UUGFEquipmentDefinition::UUGFEquipmentDefinition()
+{
+    InstanceClass = UWeaponInstance::StaticClass();
+
+    SlotType = Weapon::Slot::Primary;
+    ActorClass = AWeaponActor::StaticClass();
+    ActiveSocketName = FName("hand_r");
+    MaxDurability = 100;
+}
---
+++ Source/UniversalGameFramework/Public/Data/UGFEquipmentDefinition.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UGFItemDefinition.h"
+#include "Interfaces/WeaponDataInterface.h"
+#include "UGFEquipmentDefinition.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class UNIVERSALGAMEFRAMEWORK_API UUGFEquipmentDefinition : public UUGFItemDefinition,
+    public IWeaponDataInterface
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FGameplayTag SlotType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (MustImplement = "WeaponActorInterface"))
+    TSubclassOf<AActor> ActorClass;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName ActiveSocketName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName InActiveSocketName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxDurability;
+
+public:
+    UUGFEquipmentDefinition();
+
+    /* WeaponDataInterface */
+
+    virtual FGameplayTag GetSlotType_Implementation() const override { return SlotType; }
+    virtual TSubclassOf<AActor> GetActorClass_Implementation() const override { return ActorClass; }
+    virtual FName GetActiveSocketName_Implementation() const override { return ActiveSocketName; }
+    virtual FName GetInActiveSocketName_Implementation() const override { return InActiveSocketName; }
+    virtual float GetMaxDurability_Implementation() const override { return MaxDurability; }
+};
