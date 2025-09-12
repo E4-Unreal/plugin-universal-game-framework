ed72b0f688db5780b11514bfcb4176de4bbe5412
이정훈 <eu4ng97@gmail.com>
2025-09-12 17:06:29 +0900

    Feat: DraggableWidgetBase 클래스에서 DroppableWidgetBase 분리

---
+++ Source/SlotManager/Private/Widgets/DroppableWidgetBase.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/DroppableWidgetBase.h"
+
+#include "Blueprint/DragDropOperation.h"
+
+bool UDroppableWidgetBase::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent,
+                                        UDragDropOperation* InOperation)
+{
+    if (InOperation && InOperation->DefaultDragVisual)
+    {
+        if (UUserWidget* DraggedWidget = Cast<UUserWidget>(InOperation->DefaultDragVisual))
+        {
+            OnWidgetDrop(DraggedWidget);
+        }
+    }
+
+    return Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+}
+
+
+void UDroppableWidgetBase::OnWidgetDrop(UUserWidget* DropWidget)
+{
+    // DropWidget 클래스에 따라 이벤트 처리
+}
---
+++ Source/SlotManager/Public/Widgets/DroppableWidgetBase.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DroppableWidgetBase.generated.h"
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class SLOTMANAGER_API UDroppableWidgetBase : public UUserWidget
+{
+    GENERATED_BODY()
+
+protected:
+    /* UserWidget */
+
+    virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+
+    /* API */
+
+    virtual void OnWidgetDrop(UUserWidget* DropWidget);
+};
