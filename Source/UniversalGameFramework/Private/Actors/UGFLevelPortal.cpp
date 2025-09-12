75be5242962c8feb1b5dced566598993a9318812
이정훈 <eu4ng97@gmail.com>
2025-09-12 22:17:43 +0900

    Feat: UGFLevelPortal 클래스 추가

---
+++ Source/UniversalGameFramework/Private/Actors/UGFLevelPortal.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/UGFLevelPortal.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Subsystems/CommonWidgetManagerSubsystem.h"
+#include "Widgets/Popup/CommonPopupWidgetBase.h"
+
+
+AUGFLevelPortal::AUGFLevelPortal(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    LevelName = NSLOCTEXT("UGF", "LevelName", "Destination");
+    ConfirmTitle = NSLOCTEXT("UGF", "MoveConfirmTitle", "Notice");
+    ConfirmMessage = NSLOCTEXT("UGF", "MoveConfirmMessage", "Do you want to move to {0}");
+}
+
+void AUGFLevelPortal::Interact_Implementation(AActor* Interactor)
+{
+    Super::Interact_Implementation(Interactor);
+
+    if (APawn* Pawn = Cast<APawn>(Interactor))
+    {
+        if (APlayerController* PlayerController = Cast<APlayerController>(Pawn->GetController()))
+        {
+            RequestConfirmPopup(PlayerController);
+        }
+    }
+}
+
+void AUGFLevelPortal::RequestConfirmPopup(APlayerController* PlayerController)
+{
+    if (UCommonWidgetManagerSubsystem* Subsystem = GetGameInstance()->GetSubsystem<UCommonWidgetManagerSubsystem>())
+    {
+        FButtonClickedDelegate Delegate;
+        Delegate.BindDynamic(this, &ThisClass::MoveToLevel);
+
+        Subsystem->ShowConfirmWidget(PlayerController, ConfirmTitle, FText::Format(ConfirmMessage, LevelName), Delegate);
+    }
+}
+
+void AUGFLevelPortal::MoveToLevel()
+{
+    if (!Level.IsNull())
+    {
+        UGameplayStatics::OpenLevelBySoftObjectPtr(GetWorld(), Level);
+    }
+}
---
+++ Source/UniversalGameFramework/Public/Actors/UGFLevelPortal.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/InteractableActor.h"
+#include "UGFLevelPortal.generated.h"
+
+UCLASS()
+class UNIVERSALGAMEFRAMEWORK_API AUGFLevelPortal : public AInteractableActor
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    FText LevelName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSoftObjectPtr<UWorld> Level;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    FText ConfirmTitle;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    FText ConfirmMessage;
+
+public:
+    AUGFLevelPortal(const FObjectInitializer& ObjectInitializer);
+
+    /* InteractableInterface */
+
+    virtual void Interact_Implementation(AActor* Interactor) override;
+
+protected:
+    virtual void RequestConfirmPopup(APlayerController* PlayerController);
+
+    UFUNCTION()
+    virtual void MoveToLevel();
+};
