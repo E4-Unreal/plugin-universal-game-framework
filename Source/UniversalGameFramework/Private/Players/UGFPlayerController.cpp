7f559fd4509a4c647ba7144db526ac99498c7ce8
이정훈 <eu4ng97@gmail.com>
2025-09-12 21:32:30 +0900

    Feat: 플레이어 컨트롤러 클릭 이벤트 활성화

--- Source/UniversalGameFramework/Private/Players/UGFPlayerController.cpp
+++ Source/UniversalGameFramework/Private/Players/UGFPlayerController.cpp
@@ -14,6 +14,7 @@ AUGFPlayerController::AUGFPlayerController(const FObjectInitializer& ObjectIniti
     /* APlayerController */
 
     bEnableMouseOverEvents = true;
+    bEnableClickEvents = true;
 
     /* WidgetManager */
 
