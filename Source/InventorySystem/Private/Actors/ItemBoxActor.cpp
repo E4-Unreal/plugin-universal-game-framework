74478ed43559ec77e5121c8ea2a58dfd684e65e6
이정훈 <eu4ng97@gmail.com>
2025-09-13 12:43:30 +0900

    Feat: ItemBoxActor 클래스 추가

---
+++ Source/InventorySystem/Private/Actors/ItemBoxActor.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/ItemBoxActor.h"
+
+#include "InventorySystemFunctionLibrary.h"
+
+
+AItemBoxActor::AItemBoxActor(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    SpawnOffset = FVector(0, 0, 100);
+    ImpulseAngle = 90;
+    ImpulseStrength = 2000;
+}
+
+void AItemBoxActor::Interact_Implementation(AActor* Interactor)
+{
+    Super::Interact_Implementation(Interactor);
+
+    for (const auto& ItemInstance : ItemInstances)
+    {
+        SpawnItem(ItemInstance);
+    }
+}
+
+AActor* AItemBoxActor::SpawnItem(UDataInstanceBase* InItemInstance)
+{
+    if (AActor* SpawnedItemActor = UInventorySystemFunctionLibrary::SpawnItemActor(this, ItemActorClass, InItemInstance, SpawnOffset))
+    {
+        UInventorySystemFunctionLibrary::ImpulseActor(SpawnedItemActor, ImpulseAngle, ImpulseStrength);
+
+        return SpawnedItemActor;
+    }
+
+    return nullptr;
+}
