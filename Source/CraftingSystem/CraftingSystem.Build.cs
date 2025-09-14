9811e334f6772e04f31670cb74dc1943133cca94
이정훈 <eu4ng97@gmail.com>
2025-09-14 15:41:07 +0900

    Chore: CraftingSystem 모듈 추가

---
+++ Source/CraftingSystem/CraftingSystem.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class CraftingSystem : ModuleRules
+{
+    public CraftingSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/CraftingSystem/Private/CraftingSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "CraftingSystem.h"
+
+#define LOCTEXT_NAMESPACE "FCraftingSystemModule"
+
+void FCraftingSystemModule::StartupModule()
+{
+    
+}
+
+void FCraftingSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCraftingSystemModule, CraftingSystem)
