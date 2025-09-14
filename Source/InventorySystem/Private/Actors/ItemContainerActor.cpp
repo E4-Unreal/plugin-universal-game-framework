09012ab194861a8a47eedd0f9a02e4fc4759465c
이정훈 <eu4ng97@gmail.com>
2025-09-14 16:30:48 +0900

    Fix: ItemBoxActor 클래스를 ItemContainerActor 클래스로 변경

---
+++ Source/InventorySystem/Private/Actors/ItemContainerActor.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/ItemContainerActor.h"
+
+#include "Components/InteractableComponent.h"
+#include "GameplayTags/InventoryGameplayTags.h"
+#include "Components/ItemActorComponent.h"
+
+const FName AItemContainerActor::ItemContainerName(TEXT("ItemContainer"));
+
+AItemContainerActor::AItemContainerActor(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    /* DisplayMesh */
+
+    GetDisplayMesh()->SetSimulatePhysics(true);
+
+    /* InteractableComponent */
+
+    GetInteractableComponent()->InteractionType = Interaction::Open::Box;
+
+    /* ItemContainer */
+
+    ItemContainer = CreateDefaultSubobject<UItemContainerComponent>(ItemContainerName);
+}
+
+void AItemContainerActor::Interact_Implementation(AActor* Interactor)
+{
+    Super::Interact_Implementation(Interactor);
+
+    GetItemContainer()->SpawnItems();
+}
+
+void AItemContainerActor::SetItemInstances_Implementation(const TArray<UDataInstanceBase*>& NewItemsInstances)
+{
+    GetItemContainer()->SetItems(NewItemsInstances);
+}
