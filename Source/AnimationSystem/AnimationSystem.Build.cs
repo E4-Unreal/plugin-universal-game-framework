65aa907011d58c93450a04bd9ff3bf8c0addfa8f
이정훈 <eu4ng97@gmail.com>
2025-09-14 21:37:22 +0900

    Chore: AnimationSystem 모듈 추가

---
+++ Source/AnimationSystem/AnimationSystem.Build.cs
@@ -0,0 +1,28 @@
+﻿using UnrealBuildTool;
+
+public class AnimationSystem : ModuleRules
+{
+    public AnimationSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+
+                "UniversalUtilities"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/AnimationSystem/Private/AnimationSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "AnimationSystem.h"
+
+#define LOCTEXT_NAMESPACE "FAnimationSystemModule"
+
+void FAnimationSystemModule::StartupModule()
+{
+    
+}
+
+void FAnimationSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FAnimationSystemModule, AnimationSystem)
