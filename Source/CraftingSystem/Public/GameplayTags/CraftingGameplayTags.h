2589cb05d30a41ec9ad2512c12b6b569d6765d6d
이정훈 <eu4ng97@gmail.com>
2025-09-14 23:11:03 +0900

    Feat: Crafting 전용 게임플레이 태그 추가

---
+++ Source/CraftingSystem/Public/GameplayTags/CraftingGameplayTags.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace CraftingSystem
+{
+    namespace Interaction
+    {
+        CRAFTINGSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Woodcutting)
+        CRAFTINGSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Mining)
+        CRAFTINGSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Fishing)
+    }
+}
+
+using namespace CraftingSystem;
