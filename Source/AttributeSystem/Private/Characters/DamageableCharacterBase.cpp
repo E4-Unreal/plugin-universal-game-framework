53de4bb31c76a210c877dc9a101d50f0d83213ad
이정훈 <eu4ng97@gmail.com>
2025-09-14 12:49:29 +0900

    Feat: DamageableCharacterBase 클래스 추가

---
+++ Source/AttributeSystem/Private/Characters/DamageableCharacterBase.cpp
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/DamageableCharacterBase.h"
+
+#include "Components/AttributeSystemComponent.h"
+#include "GameplayTags/AttributeGameplayTags.h"
+
+const FName ADamageableCharacterBase::AttributeSystemName(TEXT("AttributeSystem"));
+
+ADamageableCharacterBase::ADamageableCharacterBase(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    /* SocketManager */
+
+    AttributeSystem = CreateDefaultSubobject<UAttributeSystemComponent>(AttributeSystemName);
+}
+
+void ADamageableCharacterBase::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    if (GetAttributeSystem())
+    {
+        GetAttributeSystem()->OnAttributeValueChanged.AddDynamic(this, &ThisClass::OnAttributeValueChanged);
+        GetAttributeSystem()->OnDamaged.AddDynamic(this, &ThisClass::OnDamaged);
+        GetAttributeSystem()->OnHealed.AddDynamic(this, &ThisClass::OnHealed);
+        GetAttributeSystem()->OnDead.AddDynamic(this, &ThisClass::OnDead);
+        GetAttributeSystem()->OnRevived.AddDynamic(this, &ThisClass::OnRevived);
+    }
+}
+
+float ADamageableCharacterBase::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
+                                       class AController* EventInstigator, AActor* DamageCauser)
+{
+    float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+    GetAttributeSystem()->TakeDamage(ActualDamage, DamageEvent, EventInstigator, DamageCauser);
+
+    return ActualDamage;
+}
+
+void ADamageableCharacterBase::OnAttributeValueChanged(FGameplayTag AttributeType, float OldValue, float NewValue)
+{
+
+}
+
+void ADamageableCharacterBase::OnDamaged(float Value)
+{
+
+}
+
+void ADamageableCharacterBase::OnHealed(float Value)
+{
+
+}
+
+void ADamageableCharacterBase::OnDead()
+{
+
+}
+
+void ADamageableCharacterBase::OnRevived()
+{
+
+}
