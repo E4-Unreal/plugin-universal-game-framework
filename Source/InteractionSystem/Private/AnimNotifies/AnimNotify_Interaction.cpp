aa32eddcfc708cbee6d74bcc1e846c3b5c6a2c54
이정훈 <eu4ng97@gmail.com>
2025-09-14 22:39:36 +0900

    Feat: AnimNotify_Interaction 클래스 추가

---
+++ Source/InteractionSystem/Private/AnimNotifies/AnimNotify_Interaction.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifies/AnimNotify_Interaction.h"
+
+#include "Components/InteractionSystemComponent.h"
+
+void UAnimNotify_Interaction::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                     const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    if (auto Owner = MeshComp->GetOwner())
+    {
+        if (auto InteractionSystem = Owner->GetComponentByClass<UInteractionSystemComponent>())
+        {
+            InteractionSystem->TryInteract();
+        }
+    }
+}
---
+++ Source/InteractionSystem/Public/AnimNotifies/AnimNotify_Interaction.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_Interaction.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class INTERACTIONSYSTEM_API UAnimNotify_Interaction : public UAnimNotify
+{
+    GENERATED_BODY()
+
+public:
+    /* AnimNotify */
+
+    virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
