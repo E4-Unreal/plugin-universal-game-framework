2230908ccec40f4eaeee3b45d2494b498cea7403
이정훈 <eu4ng97@gmail.com>
2025-09-16 05:05:42 +0900

    Feat: DataAssetRegistrySubsystem 관련 클래스 추가

---
+++ Source/DataManager/Private/Settings/DataManagerSettings.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Settings/DataManagerSettings.h"
+
+UDataManagerSettings::UDataManagerSettings()
+{
+    CategoryName = "Plugins";
+    SectionName = "DataManager";
+}
---
+++ Source/DataManager/Public/Objects/DataAssetBuilder.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "DataAssetBuilder.generated.h"
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class DATAMANAGER_API UDataAssetBuilder : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    /* API */
+
+    virtual TSubclassOf<UDataAsset> GetDataAssetClass() const { return nullptr; }
+    virtual UDataAsset* CreateDataAsset(UDataTable* DataTable, int32 ID) { return nullptr; }
+    virtual bool UpdateDataAsset(UDataTable* DataTable, int32 ID, UDataAsset* DataAsset) { return false; }
+};
