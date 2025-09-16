7da02186659b3f8764858eb3df82376035989f72
이정훈 <eu4ng97@gmail.com>
2025-09-17 05:43:00 +0900

    Feat: DataActorInterface 인터페이스 추가

---
+++ Source/DataManager/Private/Interfaces/DataActorInterface.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/DataActorInterface.h"
+
+#include "Interfaces/DataInstanceInterface.h"
+#include "Interfaces/DataInterface.h"
+
+
+// Add default functionality here for any IDataActorInterface functions that are not pure virtual.
+UObject* IDataActorInterface::GetDataInstance_Implementation() const
+{
+    TArray<UObject*> DataInstances = Execute_GetDataInstances(CastChecked<UObject>(this));
+
+    return !DataInstances.IsEmpty() ? DataInstances[0] : nullptr;
+}
+
+void IDataActorInterface::SetDataInstance_Implementation(UObject* NewDataInstance)
+{
+    Execute_SetDataInstances(CastChecked<UObject>(this), { NewDataInstance });
+}
+
+UDataAsset* IDataActorInterface::GetDataAsset_Implementation() const
+{
+    UObject* DataInstance = Execute_GetDataInstance(CastChecked<UObject>(this));
+    if (DataInstance && DataInstance->Implements<UDataInstanceInterface>())
+    {
+        return IDataInstanceInterface::Execute_GetData(DataInstance);
+    }
+
+    return nullptr;
+}
+
+void IDataActorInterface::SetData_Implementation(UDataAsset* NewData)
+{
+    if (NewData && NewData->Implements<UDataInterface>())
+    {
+        if (UObject* NewDataInstance = IDataInterface::Execute_CreateDataInstance(NewData))
+        {
+            Execute_SetDataInstance(CastChecked<UObject>(this), NewDataInstance);
+        }
+    }
+}
+
---
+++ Source/DataManager/Public/Interfaces/DataActorInterface.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "DataActorInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class UDataActorInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class DATAMANAGER_API IDataActorInterface
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    TArray<UObject*> GetDataInstances() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetDataInstances(const TArray<UObject*>& NewDataInstances);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    UObject* GetDataInstance() const;
+    virtual UObject* GetDataInstance_Implementation() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetDataInstance(UObject* NewDataInstance);
+    virtual void SetDataInstance_Implementation(UObject* NewDataInstance);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    UDataAsset* GetData() const;
+    virtual UDataAsset* GetDataAsset_Implementation() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetData(UDataAsset* NewData);
+    virtual void SetData_Implementation(UDataAsset* NewData);
+};
