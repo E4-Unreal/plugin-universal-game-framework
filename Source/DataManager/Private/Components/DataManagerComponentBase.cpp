c201ffdac5ffab91061a0edb69514c9c968bd35b
이정훈 <eu4ng97@gmail.com>
2025-09-17 06:44:28 +0900

    Feat: DataManagerComponent 클래스 추가

---
+++ Source/DataManager/Private/Components/DataManagerComponentBase.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/DataManagerComponentBase.h"
+
+UDataManagerComponentBase::UDataManagerComponentBase()
+{
+    bWantsInitializeComponent = true;
+}
+
+void UDataManagerComponentBase::InitializeComponent()
+{
+    Super::InitializeComponent();
+
+    Refresh();
+}
+
+void UDataManagerComponentBase::Refresh_Implementation()
+{
+}
