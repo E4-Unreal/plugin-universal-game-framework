a37868f88396024a06ea6d6cb655ca87e571665a
이정훈 <eu4ng97@gmail.com>
2025-09-19 16:57:06 +0900

    Feat: TargetWidgetInterface 인터페이스 추가

---
+++ Source/WidgetManager/Public/Interfaces/TargetWidgetInterface.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "TargetWidgetInterface.generated.h"
+
+/**
+ * 플레이어가 아닌 다른 액터에 대한 정보를 표시하기 위한 위젯 인터페이스
+ */
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class UTargetWidgetInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class WIDGETMANAGER_API ITargetWidgetInterface
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void SetTargetActor(AActor* NewTargetActor);
+};
