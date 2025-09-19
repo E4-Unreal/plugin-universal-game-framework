5168b43bf4f54e4443ff0a7c26c18356988dfae9
이정훈 <eu4ng97@gmail.com>
2025-09-19 18:48:48 +0900

    Chore: InputManager 모듈 추가

---
+++ Source/InputManager/Private/InputManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "InputManager.h"
+
+#define LOCTEXT_NAMESPACE "FInputManagerModule"
+
+void FInputManagerModule::StartupModule()
+{
+    
+}
+
+void FInputManagerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FInputManagerModule, InputManager)
