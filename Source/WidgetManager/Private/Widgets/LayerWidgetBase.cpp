97e485f4ff0bc6930388352c05f2b6ffed3a4db7
이정훈 <eu4ng97@gmail.com>
2025-09-19 17:40:50 +0900

    Feat: LayerWidgetBase 클래스 추가

---
+++ Source/WidgetManager/Private/Widgets/LayerWidgetBase.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/LayerWidgetBase.h"
+
+#include "GameplayTags/WidgetManagerGameplayTags.h"
+
+ULayerWidgetBase::ULayerWidgetBase(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    LayerTag = WidgetManager::UI::Layer::Game::Overlay;
+}
