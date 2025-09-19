0bcd3aa1aa4dd04e0612f9b4d082d960bdf0965a
이정훈 <eu4ng97@gmail.com>
2025-09-19 17:10:10 +0900

    Feat: LayerWidgetInterface 인터페이스 추가

---
+++ Source/WidgetManager/Public/Interfaces/LayerWidgetInterface.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/Interface.h"
+#include "LayerWidgetInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class ULayerWidgetInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class WIDGETMANAGER_API ILayerWidgetInterface
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    FGameplayTag GetLayerTag() const;
+};
