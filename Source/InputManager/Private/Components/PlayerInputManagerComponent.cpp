88d4df1dc365c11739034ea4c0d3644a3fe0052c
이정훈 <eu4ng97@gmail.com>
2025-09-21 15:45:38 +0900

    Feat: PlayerInputManagerComponent 클래스 추가

---
+++ Source/InputManager/Private/Components/PlayerInputManagerComponent.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/PlayerInputManagerComponent.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+
+UPlayerInputManagerComponent::UPlayerInputManagerComponent()
+{
+}
+
+void UPlayerInputManagerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    AddDefaultMappingContexts();
+}
+
+void UPlayerInputManagerComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+    RemoveDefaultMappingContexts();
+
+    Super::OnComponentDestroyed(bDestroyingHierarchy);
+}
+
+UEnhancedInputLocalPlayerSubsystem* UPlayerInputManagerComponent::GetEnhancedInputLocalPlayerSubsystem() const
+{
+    return ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(CastChecked<APlayerController>(GetOwner())->GetLocalPlayer());
+}
+
+void UPlayerInputManagerComponent::AddDefaultMappingContexts()
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        for (UInputMappingContext* DefaultMappingContext : DefaultMappingContexts)
+        {
+            Subsystem->AddMappingContext(DefaultMappingContext, 0);
+        }
+    }
+}
+
+void UPlayerInputManagerComponent::RemoveDefaultMappingContexts()
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        for (UInputMappingContext* DefaultMappingContext : DefaultMappingContexts)
+        {
+            Subsystem->RemoveMappingContext(DefaultMappingContext);
+        }
+    }
+}
---
+++ Source/InputManager/Public/Components/PlayerInputManagerComponent.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerInputManagerComponent.generated.h"
+
+class UInputMappingContext;
+class UEnhancedInputLocalPlayerSubsystem;
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class INPUTMANAGER_API UPlayerInputManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TArray<TObjectPtr<UInputMappingContext>> DefaultMappingContexts;
+
+public:
+    UPlayerInputManagerComponent();
+
+    /* ActorComponent */
+
+    virtual void BeginPlay() override;
+    virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
+protected:
+    /* API */
+
+    UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputLocalPlayerSubsystem() const;
+
+    virtual void AddDefaultMappingContexts();
+    virtual void RemoveDefaultMappingContexts();
+};
