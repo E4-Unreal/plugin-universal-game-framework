cb33f33e388e500a85ed9ac49ae8bf7771795dd0
이정훈 <eu4ng97@gmail.com>
2025-09-25 08:24:33 +0900

    Feat: DataManagerSubsystem 클래스 추가

---
+++ Source/DataManager/Private/Subsystems/DataManagerSubsystem.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystems/DataManagerSubsystem.h"
+
+#include "Engine/AssetManager.h"
+
+bool UDataManagerSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+    TArray<UClass*> ChildClasses;
+    GetDerivedClasses(GetClass(), ChildClasses, false);
+
+    return ChildClasses.Num() == 0;
+}
+
+UDataAsset* UDataManagerSubsystem::GetDataAsset(FName AssetType, int32 ID) const
+{
+    FPrimaryAssetId AssetID = FPrimaryAssetId(AssetType, FName(FString::FromInt(ID)));
+
+    return UAssetManager::Get().GetPrimaryAssetObject<UDataAsset>(AssetID);
+}
+
+TArray<UDataAsset*> UDataManagerSubsystem::GetDataAssets(FName AssetType) const
+{
+    TArray<UObject*> Objects;
+    UAssetManager::Get().GetPrimaryAssetObjectList(AssetType, Objects);
+
+    TArray<UDataAsset*> DataAssets;
+    DataAssets.Reserve(Objects.Num());
+    for (UObject* Object : Objects)
+    {
+        if (UDataAsset* DataAsset = Cast<UDataAsset>(Object))
+        {
+            DataAssets.Emplace(DataAsset);
+        }
+    }
+
+    return DataAssets;
+}
---
+++ Source/DataManager/Public/Subsystems/DataManagerSubsystem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "DataManagerSubsystem.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class DATAMANAGER_API UDataManagerSubsystem : public UEngineSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    /* Subsystem */
+
+    virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+    /* API */
+
+    UFUNCTION(BlueprintPure)
+    virtual UDataAsset* GetDataAsset(FName AssetType, int32 ID) const;
+
+    UFUNCTION(BlueprintPure)
+    virtual TArray<UDataAsset*> GetDataAssets(FName AssetType) const;
+};
