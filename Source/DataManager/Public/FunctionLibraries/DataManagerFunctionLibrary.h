38ccb4ad7effc445ab17c0303b2284858c7a0f68
이정훈 <eu4ng97@gmail.com>
2025-09-29 11:02:38 +0900

    Feat: DataManagerFunctionLibrary 클래스 추가

---
+++ Source/DataManager/Public/FunctionLibraries/DataManagerFunctionLibrary.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DataManagerFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DATAMANAGER_API UDataManagerFunctionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+};
