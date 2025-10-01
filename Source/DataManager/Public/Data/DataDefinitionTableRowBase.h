8c8226dd875cd84461c6ecd53ebedf1fb76f471b
이정훈 <eu4ng97@gmail.com>
2025-10-02 04:34:54 +0900

    Feat: DataDefinitionTableRowBase 구조체 추가

---
+++ Source/DataManager/Public/Data/DataDefinitionTableRowBase.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataDefinitionTableRowBase.generated.h"
+
+USTRUCT(BlueprintType)
+struct DATAMANAGER_API FDataDefinitionTableRowBase : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText DisplayText;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText Description;
+};
