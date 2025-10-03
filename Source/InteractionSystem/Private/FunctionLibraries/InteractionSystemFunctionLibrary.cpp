b6c88a750d2df9d84c213989cad540d94bec9743
이정훈 <eu4ng97@gmail.com>
2025-10-04 07:14:34 +0900

    Feat: InteractionSystemFunctionLibrary 클래스 추가

---
+++ Source/InteractionSystem/Private/FunctionLibraries/InteractionSystemFunctionLibrary.cpp
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FunctionLibraries/InteractionSystemFunctionLibrary.h"
+
+#include "Interfaces/InteractableInterface.h"
+
+UObject* UInteractionSystemFunctionLibrary::GetInteractableObject(AActor* TargetActor)
+{
+    UObject* InteractableObject = nullptr;
+
+    if (TargetActor)
+    {
+        if (TargetActor->Implements<UInteractableInterface>())
+        {
+            InteractableObject = TargetActor;
+        }
+        else
+        {
+            InteractableObject = TargetActor->FindComponentByInterface<UInteractableInterface>();
+        }
+    }
+
+    return InteractableObject;
+}
+
+FGameplayTag UInteractionSystemFunctionLibrary::GetInteractionType(AActor* TargetActor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        return IInteractableInterface::Execute_GetInteractionType(InteractableObject);
+    }
+
+    return FGameplayTag::EmptyTag;
+}
+
+FText UInteractionSystemFunctionLibrary::GetInteractionMessage(AActor* TargetActor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        return IInteractableInterface::Execute_GetInteractionMessage(InteractableObject);
+    }
+
+    return FText::GetEmpty();
+}
+
+bool UInteractionSystemFunctionLibrary::CanInteract(AActor* TargetActor, AActor* Interactor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        return IInteractableInterface::Execute_CanInteract(InteractableObject, Interactor);
+    }
+
+    return false;
+}
+
+void UInteractionSystemFunctionLibrary::Interact(AActor* TargetActor, AActor* Interactor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        IInteractableInterface::Execute_Interact(InteractableObject, Interactor);
+    }
+}
+
+void UInteractionSystemFunctionLibrary::CancelInteract(AActor* TargetActor, AActor* Interactor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        IInteractableInterface::Execute_CancelInteract(InteractableObject, Interactor);
+    }
+}
+
+bool UInteractionSystemFunctionLibrary::CanSelect(AActor* TargetActor, AActor* Interactor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        return IInteractableInterface::Execute_CanSelect(InteractableObject, Interactor);
+    }
+
+    return false;
+}
+
+void UInteractionSystemFunctionLibrary::Select(AActor* TargetActor, AActor* Interactor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        IInteractableInterface::Execute_Select(InteractableObject, Interactor);
+    }
+}
+
+void UInteractionSystemFunctionLibrary::Deselect(AActor* TargetActor, AActor* Interactor)
+{
+    if (auto InteractableObject = GetInteractableObject(TargetActor))
+    {
+        IInteractableInterface::Execute_Deselect(InteractableObject, Interactor);
+    }
+}
---
+++ Source/InteractionSystem/Public/FunctionLibraries/InteractionSystemFunctionLibrary.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "InteractionSystemFunctionLibrary.generated.h"
+
+struct FGameplayTag;
+
+/**
+ *
+ */
+UCLASS()
+class INTERACTIONSYSTEM_API UInteractionSystemFunctionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintPure)
+    static UObject* GetInteractableObject(AActor* TargetActor);
+
+    UFUNCTION(BlueprintPure)
+    static FGameplayTag GetInteractionType(AActor* TargetActor);
+
+    UFUNCTION(BlueprintPure)
+    static FText GetInteractionMessage(AActor* TargetActor);
+
+    UFUNCTION(BlueprintPure)
+    static bool CanInteract(AActor* TargetActor, AActor* Interactor);
+
+    UFUNCTION(BlueprintCallable)
+    static void Interact(AActor* TargetActor, AActor* Interactor);
+
+    UFUNCTION(BlueprintCallable)
+    static void CancelInteract(AActor* TargetActor, AActor* Interactor);
+
+    UFUNCTION(BlueprintPure)
+    static bool CanSelect(AActor* TargetActor, AActor* Interactor);
+
+    UFUNCTION(BlueprintCallable)
+    static void Select(AActor* TargetActor, AActor* Interactor);
+
+    UFUNCTION(BlueprintCallable)
+    static void Deselect(AActor* TargetActor, AActor* Interactor);
+};
