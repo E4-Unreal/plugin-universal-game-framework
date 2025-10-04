fadda78a2a8eb30cfa65306330f7c06df60028c8
이정훈 <eu4ng97@gmail.com>
2025-10-05 06:04:45 +0900

    Feat: EditableNumericText 위젯 클래스 추가

---
+++ Source/WidgetManager/Private/Widgets/EditableNumericText.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/EditableNumericText.h"
+
+#include "Kismet/KismetStringLibrary.h"
+
+UEditableNumericText::UEditableNumericText()
+{
+    SetSelectAllTextWhenFocused(true);
+
+    bNumeric = true;
+    MinValue = 0;
+    MaxValue = INT_MAX;
+
+    OnTextChanged.AddDynamic(this, &ThisClass::NativeOnTextChanged);
+}
+
+void UEditableNumericText::OnWidgetRebuilt()
+{
+    Super::OnWidgetRebuilt();
+
+    if (bNumeric)
+    {
+        SetText(FText::FromString(FString::FromInt(MinValue)));
+    }
+    else
+    {
+        SetText(FText::GetEmpty());
+    }
+}
+
+void UEditableNumericText::SelectAllText()
+{
+    if (MyEditableText.IsValid())
+    {
+        MyEditableText->SelectAllText();
+    }
+}
+
+int32 UEditableNumericText::GetValue() const
+{
+    if (GetText().IsNumeric())
+    {
+        return FCString::Atoi(*GetText().ToString());
+    }
+
+    return MinValue;
+}
+
+void UEditableNumericText::SetValue(int32 NewValue)
+{
+    SetText(FText::FromString(FString::FromInt(NewValue)));
+}
+
+void UEditableNumericText::NativeOnTextChanged(const FText& NewText)
+{
+    if (bNumeric)
+    {
+        FString String = NewText.ToString();
+        if (!String.IsNumeric())
+        {
+            TArray<FString> CharacterArray = UKismetStringLibrary::GetCharacterArrayFromString(String);
+            String.Empty();
+            for (FString Character : CharacterArray)
+            {
+                if (Character.IsNumeric())
+                {
+                    String += Character;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if (!String.IsEmpty())
+        {
+            auto Value = FMath::Clamp(FCString::Atoi(*String), MinValue, MaxValue);
+            String = FString::FromInt(Value);
+        }
+
+        SetText(FText::FromString(String));
+    }
+}
---
+++ Source/WidgetManager/Public/Widgets/EditableNumericText.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/EditableText.h"
+#include "EditableNumericText.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class WIDGETMANAGER_API UEditableNumericText : public UEditableText
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    bool bNumeric;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    int32 MaxValue;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    int32 MinValue;
+
+public:
+    UEditableNumericText();
+
+    /* UWidget */
+
+    virtual void OnWidgetRebuilt() override;
+
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    void SelectAllText();
+
+    UFUNCTION(BlueprintPure)
+    virtual int32 GetValue() const;
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetValue(int32 NewValue);
+
+protected:
+    /* EditableText */
+
+    UFUNCTION()
+    virtual void NativeOnTextChanged(const FText& NewText);
+};
