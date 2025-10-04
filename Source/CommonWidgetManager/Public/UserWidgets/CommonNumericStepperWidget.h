f1cd3b97ecd58be589879566c56c46cc821dcc70
이정훈 <eu4ng97@gmail.com>
2025-10-05 08:38:50 +0900

    Feat: CommonNumericStepperWidget 클래스 추가

---
+++ Source/CommonWidgetManager/Public/UserWidgets/CommonNumericStepperWidget.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CommonNumericStepperWidget.generated.h"
+
+class UEditableNumericText;
+class UCommonButtonBase;
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class COMMONWIDGETMANAGER_API UCommonNumericStepperWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UEditableNumericText> EditableNumericText;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UCommonButtonBase> IncreaseButton;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UCommonButtonBase> DecreaseButton;
+
+public:
+    /* UserWidget */
+
+    virtual void NativeOnInitialized() override;
+
+    /* Components */
+
+    FORCEINLINE UEditableNumericText* GetEditableNumericText() const { return EditableNumericText; }
+    FORCEINLINE UCommonButtonBase* GetIncreaseButton() const { return IncreaseButton; }
+    FORCEINLINE UCommonButtonBase* GetDecreaseButton() const { return DecreaseButton; }
+
+protected:
+    UFUNCTION()
+    virtual void OnIncreaseButtonClicked();
+
+    UFUNCTION()
+    virtual void OnDecreaseButtonClicked();
+};
