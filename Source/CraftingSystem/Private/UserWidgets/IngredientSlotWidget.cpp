ebb460c3a872985ff3a3ace9e67fcee9556fcc63
이정훈 <eu4ng97@gmail.com>
2025-10-05 07:20:28 +0900

    Feat: IngredientSlotWidget 클래스 추가

---
+++ Source/CraftingSystem/Private/UserWidgets/IngredientSlotWidget.cpp
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UserWidgets/IngredientSlotWidget.h"
+
+#include "Components/Image.h"
+#include "Components/InventoryComponent.h"
+#include "Components/TextBlock.h"
+#include "FunctionLibraries/SlotDataFunctionLibrary.h"
+
+void UIngredientSlotWidget::SynchronizeProperties()
+{
+    Super::SynchronizeProperties();
+
+#if WITH_EDITOR
+    if (IsDesignTime())
+    {
+        if (PreviewItem.Definition)
+        {
+            SetItem(PreviewItem);
+        }
+    }
+#endif
+}
+
+void UIngredientSlotWidget::SetThumbnailTexture(TSoftObjectPtr<UTexture2D> NewThumbnailTexture)
+{
+    if (GetThumbnailImage())
+    {
+        if (NewThumbnailTexture.IsNull())
+        {
+            GetThumbnailImage()->SetBrushTintColor(FSlateColor(FLinearColor::Transparent));
+        }
+        else
+        {
+            GetThumbnailImage()->SetBrushTintColor(FSlateColor(FLinearColor::White));
+            GetThumbnailImage()->SetBrushFromSoftTexture(NewThumbnailTexture);
+        }
+    }
+}
+
+void UIngredientSlotWidget::SetOwnedQuantity(int32 NewOwnedQuantity)
+{
+    NewOwnedQuantity = FMath::Min(0, NewOwnedQuantity);
+
+    if (GetOwnedQuantityTextBlock())
+    {
+        GetOwnedQuantityTextBlock()->SetText(FText::FromString(FString::FromInt(NewOwnedQuantity)));
+    }
+}
+
+void UIngredientSlotWidget::SetNeededQuantity(int32 NewNeededQuantity)
+{
+    NewNeededQuantity = FMath::Max(0, NewNeededQuantity);
+
+    if (GetNeededQuantityTextBlock())
+    {
+        GetNeededQuantityTextBlock()->SetText(FText::FromString(FString::FromInt(NewNeededQuantity)));
+    }
+}
+
+void UIngredientSlotWidget::SetItem(const FItem& NewItem)
+{
+    auto ItemDefinition = NewItem.Definition;
+    auto ThumbnailTexture = USlotDataFunctionLibrary::GetThumbnailTexture(ItemDefinition);
+    const int32 Quantity = NewItem.Quantity;
+    int32 OwnedQuantity = 0;
+    if (auto OwningPlayerPawn = GetOwningPlayerPawn())
+    {
+        if (auto Inventory = OwningPlayerPawn->GetComponentByClass<UInventoryComponent>())
+        {
+            OwnedQuantity = Inventory->GetItemQuantity(ItemDefinition);
+        }
+    }
+
+    SetThumbnailTexture(ThumbnailTexture);
+    SetOwnedQuantity(OwnedQuantity);
+    SetNeededQuantity(Quantity);
+}
---
+++ Source/CraftingSystem/Public/UserWidgets/IngredientSlotWidget.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Types/Item.h"
+#include "IngredientSlotWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class CRAFTINGSYSTEM_API UIngredientSlotWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UImage> ThumbnailImage;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UTextBlock> OwnedQuantityTextBlock;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UTextBlock> NeededQuantityTextBlock;
+
+public:
+#if WITH_EDITORONLY_DATA
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (DesignerRebuild))
+    FItem PreviewItem;
+#endif
+
+public:
+    /* Widget */
+
+    virtual void SynchronizeProperties() override;
+
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetThumbnailTexture(TSoftObjectPtr<UTexture2D> NewThumbnailTexture);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetOwnedQuantity(int32 NewOwnedQuantity);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetNeededQuantity(int32 NewNeededQuantity);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetItem(const FItem& NewItem);
+
+    /* Components */
+
+    FORCEINLINE UImage* GetThumbnailImage() const { return ThumbnailImage; }
+    FORCEINLINE UTextBlock* GetOwnedQuantityTextBlock() const { return OwnedQuantityTextBlock; }
+    FORCEINLINE UTextBlock* GetNeededQuantityTextBlock() const { return NeededQuantityTextBlock; }
+};
