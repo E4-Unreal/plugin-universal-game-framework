fd565f1c99af4fbc015fd68e9f6c145fbb208792
이정훈 <eu4ng97@gmail.com>
2025-10-05 06:20:37 +0900

    Feat: NumericStepperWidget 클래스 추가

---
+++ Source/WidgetManager/Private/UserWidgets/NumericStepperWidget.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UserWidgets/NumericStepperWidget.h"
+
+#include "Components/Button.h"
+#include "Widgets/EditableNumericText.h"
+
+void UNumericStepperWidget::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+
+    if (GetIncreaseButton())
+    {
+        GetIncreaseButton()->OnClicked.AddDynamic(this, &ThisClass::OnIncreaseButtonClicked);
+    }
+
+    if (GetDecreaseButton())
+    {
+        GetDecreaseButton()->OnClicked.AddDynamic(this, &ThisClass::OnDecreaseButtonClicked);
+    }
+}
+
+void UNumericStepperWidget::OnIncreaseButtonClicked()
+{
+    if (GetEditableNumericText())
+    {
+        GetEditableNumericText()->SetValue(GetEditableNumericText()->GetValue() + 1);
+    }
+}
+
+void UNumericStepperWidget::OnDecreaseButtonClicked()
+{
+    if (GetEditableNumericText())
+    {
+        GetEditableNumericText()->SetValue(GetEditableNumericText()->GetValue() - 1);
+    }
+}
---
+++ Source/WidgetManager/Public/UserWidgets/NumericStepperWidget.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NumericStepperWidget.generated.h"
+
+class UEditableNumericText;
+class UButton;
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class WIDGETMANAGER_API UNumericStepperWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UEditableNumericText> EditableNumericText;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UButton> IncreaseButton;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true", BindWidget))
+    TObjectPtr<UButton> DecreaseButton;
+
+public:
+    /* UserWidget */
+
+    virtual void NativeOnInitialized() override;
+
+    /* Components */
+
+    FORCEINLINE UEditableNumericText* GetEditableNumericText() const { return EditableNumericText; }
+    FORCEINLINE UButton* GetIncreaseButton() const { return IncreaseButton; }
+    FORCEINLINE UButton* GetDecreaseButton() const { return DecreaseButton; }
+
+protected:
+    UFUNCTION()
+    virtual void OnIncreaseButtonClicked();
+
+    UFUNCTION()
+    virtual void OnDecreaseButtonClicked();
+};
