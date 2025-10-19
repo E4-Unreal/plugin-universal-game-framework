367313ef79a80ed5d8b7d913a86225c0ee3e1fd2
이정훈 <eu4ng97@gmail.com>
2025-10-19 12:03:03 +0900

    Feat: SocketDataInterface 인터페이스 추가

---
+++ Source/SocketManager/Public/Interfaces/SocketDataInterface.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GameplayTagContainer.h"
+#include "SocketDataInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class USocketDataInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class SOCKETMANAGER_API ISocketDataInterface
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    FGameplayTag GetSocketType() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    FName GetSocketName() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    TSoftObjectPtr<UStaticMesh> GetStaticMesh() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    TSoftObjectPtr<USkeletalMesh> GetSkeletalMesh() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    TSoftClassPtr<AActor> GetActorClass() const;
+};
