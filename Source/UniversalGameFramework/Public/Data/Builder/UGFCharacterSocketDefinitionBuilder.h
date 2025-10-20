63c70d319169a256930b3876436636c5f113a3cb
이정훈 <eu4ng97@gmail.com>
2025-10-20 20:46:59 +0900

    Feat: UGFCharacterSocketDefinitionBuilder 클래스 추가

---
+++ Source/UniversalGameFramework/Public/Data/Builder/UGFCharacterSocketDefinitionBuilder.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/DataDefinitionBuilder.h"
+#include "UGFCharacterSocketDefinitionBuilder.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class UNIVERSALGAMEFRAMEWORK_API UUGFCharacterSocketDefinitionBuilder : public UDataDefinitionBuilder
+{
+    GENERATED_BODY()
+
+public:
+    UUGFCharacterSocketDefinitionBuilder();
+
+protected:
+    /* DataDefinitionBuilder */
+
+    virtual bool OnUpdateData(UDataDefinitionBase* Definition,
+        FDataDefinitionTableRowBase* DataDefinitionTableRow) override;
+};
