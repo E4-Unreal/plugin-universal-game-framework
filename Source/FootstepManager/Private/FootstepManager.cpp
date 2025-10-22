31acd194774067d2b5a479033bb187e0b72d0252
이정훈 <eu4ng97@gmail.com>
2025-10-23 02:40:07 +0900

    Chore: FootstepManager 모듈 추가

---
+++ Source/FootstepManager/Private/FootstepManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "FootstepManager.h"
+
+#define LOCTEXT_NAMESPACE "FFootstepManagerModule"
+
+void FFootstepManagerModule::StartupModule()
+{
+    
+}
+
+void FFootstepManagerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FFootstepManagerModule, FootstepManager)
