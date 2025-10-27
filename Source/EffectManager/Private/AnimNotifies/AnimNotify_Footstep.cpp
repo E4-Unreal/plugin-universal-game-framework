0b3c65a7c833020bc6fbf9263c01a075638d0dae
이정훈 <eu4ng97@gmail.com>
2025-10-27 19:57:43 +0900

    Feat: AnimNotify_Footstep 애님노티파이 클래스 추가

---
+++ Source/EffectManager/Private/AnimNotifies/AnimNotify_Footstep.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifies/AnimNotify_Footstep.h"
+
+#include "Components/FootstepManagerComponent.h"
+
+UAnimNotify_Footstep::UAnimNotify_Footstep()
+{
+    SocketName = "root";
+}
+
+void UAnimNotify_Footstep::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+    const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    if (auto Owner = MeshComp->GetOwner())
+    {
+        if (auto FootstepManager = Owner->GetComponentByClass<UFootstepManagerComponent>())
+        {
+            FootstepManager->SpawnEffect(SocketName);
+        }
+    }
+}
---
+++ Source/EffectManager/Public/AnimNotifies/AnimNotify_Footstep.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_Footstep.generated.h"
+
+/**
+ *
+ */
+UCLASS(Meta = (DisplayName = "Footstep"))
+class EFFECTMANAGER_API UAnimNotify_Footstep : public UAnimNotify
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName SocketName;
+
+public:
+    UAnimNotify_Footstep();
+
+    /* AnimNotify */
+
+    virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
