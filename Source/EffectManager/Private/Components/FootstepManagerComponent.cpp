55bf538eb1d3a93b9f208b9c4a8fff81b21963be
이정훈 <eu4ng97@gmail.com>
2025-10-27 19:51:27 +0900

    Feat: FootstepManagerComponent 클래스 추가

---
+++ Source/EffectManager/Private/Components/FootstepManagerComponent.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/FootstepManagerComponent.h"
+
+#include "AbilitySystemGlobals.h"
+#include "GameplayCueManager.h"
+#include "GameplayEffectTypes.h"
+#include "Data/HitEffectDefinition.h"
+#include "GameFramework/Character.h"
+#include "Settings/EffectManagerSettings.h"
+
+
+UFootstepManagerComponent::UFootstepManagerComponent()
+{
+    TraceChannel = ECC_Visibility;
+    TraceDistance = 100.0f;
+}
+
+void UFootstepManagerComponent::SetRootMesh(USkeletalMeshComponent* NewRootMesh)
+{
+    if (NewRootMesh == nullptr || RootMesh == NewRootMesh) return;
+
+    RootMesh = NewRootMesh;
+}
+
+void UFootstepManagerComponent::SpawnEffect(FName SocketName)
+{
+    // 유효성 검사
+    if (!RootMesh.IsValid() || !RootMesh->DoesSocketExist(SocketName)) return;
+
+    auto World = GetWorld();
+    if (World == nullptr) return;
+
+    // 라인 트레이스
+    FHitResult HitResult;
+    FVector TraceStart = RootMesh->GetSocketLocation(SocketName);
+    FVector TraceEnd = TraceStart - RootMesh->GetUpVector() * TraceDistance;
+    FCollisionQueryParams CollisionQueryParams;
+    CollisionQueryParams.AddIgnoredActor(GetOwner());
+    CollisionQueryParams.bReturnPhysicalMaterial = true;
+
+    World->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, TraceChannel, CollisionQueryParams);
+
+    if (!HitResult.bBlockingHit) return;
+
+    // 게임플레이 큐 처리
+    auto LocalHitEffectDefinition = GetHitEffectDefinition();
+    if (LocalHitEffectDefinition == nullptr) return;
+
+    auto GameplayCueTag = LocalHitEffectDefinition->GetGameplayCueTagByPhysicalMaterial(HitResult.PhysMaterial.Get());
+    if (!GameplayCueTag.IsValid()) return;
+
+    FGameplayCueParameters GameplayCueParameters;
+    GameplayCueParameters.EffectContext.AddHitResult(HitResult);
+    GameplayCueParameters.EffectContext.AddInstigator(GetOwner(), GetOwner());
+
+    UAbilitySystemGlobals::Get().GetGameplayCueManager()->HandleGameplayCue(HitResult.GetActor(), GameplayCueTag, EGameplayCueEvent::Executed, GameplayCueParameters);
+}
+
+void UFootstepManagerComponent::FindRootMesh()
+{
+    if (RootMesh.IsValid()) return;
+
+    USkeletalMeshComponent* FoundRootMesh = nullptr;
+
+    if (auto Character = Cast<ACharacter>(GetOwner()))
+    {
+        FoundRootMesh = Character->GetMesh();
+    }
+    else
+    {
+        FoundRootMesh = GetOwner()->FindComponentByClass<USkeletalMeshComponent>();
+    }
+
+    SetRootMesh(FoundRootMesh);
+}
+
+UHitEffectDefinition* UFootstepManagerComponent::GetHitEffectDefinition() const
+{
+    return HitEffectDefinition ? HitEffectDefinition.Get() : UEffectManagerSettings::Get()->GetFootstepEffectDefinition();
+}
---
+++ Source/EffectManager/Public/Components/FootstepManagerComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "FootstepManagerComponent.generated.h"
+
+
+class UHitEffectDefinition;
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class EFFECTMANAGER_API UFootstepManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TEnumAsByte<ECollisionChannel> TraceChannel;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    float TraceDistance;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TObjectPtr<UHitEffectDefinition> HitEffectDefinition;
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Reference", Transient)
+    TWeakObjectPtr<USkeletalMeshComponent> RootMesh;
+
+public:
+    UFootstepManagerComponent();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SetRootMesh(USkeletalMeshComponent* NewRootMesh);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void SpawnEffect(FName SocketName);
+
+protected:
+    virtual void FindRootMesh();
+
+    virtual UHitEffectDefinition* GetHitEffectDefinition() const;
+};
