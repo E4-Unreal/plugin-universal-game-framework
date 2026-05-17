3ac69bc6b7f00587d0c526e9e2f010deb00a4f88
Eu4ng <eu4ng97@gmail.com>
2026-05-17 13:40:48 +0900

    chore: MVVMExtensions 모듈 추가

---
+++ Source/MVVMExtensions/Private/MVVMExtensions.cpp
@@ -0,0 +1,17 @@
+﻿#include "MVVMExtensions.h"
+
+#define LOCTEXT_NAMESPACE "FMVVMExtensionsModule"
+
+void FMVVMExtensionsModule::StartupModule()
+{
+    
+}
+
+void FMVVMExtensionsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FMVVMExtensionsModule, MVVMExtensions)
