6200b9e6c0071f452342f537441f09ba30c06232
Eu4ng <eu4ng97@gmail.com>
2026-05-17 17:04:17 +0900

    chore: OutGameSystem 모듈 추가

---
+++ Source/OutGameSystem/Private/OutGameSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "OutGameSystem.h"
+
+#define LOCTEXT_NAMESPACE "FOutGameSystemModule"
+
+void FOutGameSystemModule::StartupModule()
+{
+    
+}
+
+void FOutGameSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FOutGameSystemModule, OutGameSystem)
