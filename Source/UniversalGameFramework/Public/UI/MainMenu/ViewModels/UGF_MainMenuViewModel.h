cce6680fbdb5ea2e2331252368d85c9aee9543a9
Eu4ng <eu4ng97@gmail.com>
2026-05-17 11:56:51 +0900

    feat: 메인 메뉴 뷰모델 클래스 생성

---
+++ Source/UniversalGameFramework/Public/UI/MainMenu/ViewModels/UGF_MainMenuViewModel.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "UGF_MainMenuViewModel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSALGAMEFRAMEWORK_API UUGF_MainMenuViewModel : public UMVVMViewModelBase
+{
+    GENERATED_BODY()
+};
