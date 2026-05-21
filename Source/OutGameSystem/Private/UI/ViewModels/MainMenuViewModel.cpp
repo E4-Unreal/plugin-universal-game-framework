8560dff633b4f8ebda2687123577bda450601266
Eu4ng <eu4ng97@gmail.com>
2026-05-21 15:52:14 +0900

    feat: MainMenuViewModel 프로퍼티 및 함수 정의 추가

--- Source/OutGameSystem/Private/UI/ViewModels/MainMenuViewModel.cpp
+++ Source/OutGameSystem/Private/UI/ViewModels/MainMenuViewModel.cpp
@@ -2,3 +2,18 @@
 
 
 #include "UI/ViewModels/MainMenuViewModel.h"
+
+void UMainMenuViewModel::HandleOnStartButtonClicked()
+{
+    // TODO
+}
+
+void UMainMenuViewModel::HandleOnOptionsButtonClicked()
+{
+    // TODO
+}
+
+void UMainMenuViewModel::HandleOnExitButtonClicked()
+{
+    // TODO
+}
--- Source/OutGameSystem/Public/UI/ViewModels/MainMenuViewModel.h
+++ Source/OutGameSystem/Public/UI/ViewModels/MainMenuViewModel.h
@@ -13,4 +13,49 @@ UCLASS()
 class OUTGAMESYSTEM_API UMainMenuViewModel : public UModelInjectableViewModelBase
 {
     GENERATED_BODY()
+
+private:
+    UPROPERTY(BlueprintReadWrite, FieldNotify, Getter, Setter, meta = (AllowPrivateAccess))
+    FText GameTitleText;
+
+    UPROPERTY(BlueprintReadWrite, FieldNotify, Getter, Setter, meta = (AllowPrivateAccess))
+    FText VersionText;
+
+public:
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    virtual void HandleOnStartButtonClicked();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void HandleOnOptionsButtonClicked();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void HandleOnExitButtonClicked();
+
+    /* Getter & Setter */
+
+    // GameTitleText
+
+    const FText& GetGameTitleText() const { return GameTitleText; }
+
+    virtual void SetGameTitleText(const FText& NewGameTitleText)
+    {
+        if (UE_MVVM_SET_PROPERTY_VALUE(GameTitleText, NewGameTitleText))
+        {
+            UE_MVVM_BROADCAST_FIELD_VALUE_CHANGED(GameTitleText);
+        }
+    }
+
+    // VersionText
+
+    const FText& GetVersionText() const { return VersionText; }
+
+    virtual void SetVersionText(const FText& NewVersionText)
+    {
+        if (UE_MVVM_SET_PROPERTY_VALUE(VersionText, NewVersionText))
+        {
+            UE_MVVM_BROADCAST_FIELD_VALUE_CHANGED(VersionText);
+        }
+    }
 };
