b6b795a5e161c08b35138f34eed81d264f3ecba6
Eu4ng <eu4ng97@gmail.com>
2026-05-21 14:09:33 +0900

    feat: UniversalGameFrameworkTests 모듈 생성

---
+++ Source/UniversalGameFrameworkTests/Private/UniversalGameFrameworkTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "UniversalGameFrameworkTests.h"
+
+#define LOCTEXT_NAMESPACE "FUniversalGameFrameworkTestsModule"
+
+void FUniversalGameFrameworkTestsModule::StartupModule()
+{
+    
+}
+
+void FUniversalGameFrameworkTestsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUniversalGameFrameworkTestsModule, UniversalGameFrameworkTests)
