abca34155d8d4630200a868664898e6f3ceab62f
Eu4ng <eu4ng97@gmail.com>
2026-05-27 22:26:55 +0900

    feat: InputManagerComponent 클래스 추가

---
+++ Source/InputManager/Private/Components/InputManagerComponent.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/InputManagerComponent.h"
+
+#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameStateBase.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+UInputManagerComponent::UInputManagerComponent()
+{
+}
+
+void UInputManagerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    AddMappingContexts(DefaultMappingContexts);
+}
+
+void UInputManagerComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+    RemoveMappingContexts(DefaultMappingContexts);
+
+    Super::OnComponentDestroyed(bDestroyingHierarchy);
+}
+
+void UInputManagerComponent::AddMappingContext(const UInputMappingContext* MappingContext, int32 Priority,
+    const FModifyContextOptions& Options)
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        Subsystem->AddMappingContext(MappingContext, Priority, Options);
+    }
+}
+
+void UInputManagerComponent::AddMappingContexts(const TArray<UInputMappingContext*>& MappingContexts, int32 Priority,
+    const FModifyContextOptions& Options)
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        for (UInputMappingContext* MappingContext : MappingContexts)
+        {
+            Subsystem->AddMappingContext(MappingContext, Priority, Options);
+        }
+    }
+}
+
+void UInputManagerComponent::RemoveMappingContext(const UInputMappingContext* MappingContext,
+                                                  const FModifyContextOptions& Options)
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        Subsystem->RemoveMappingContext(MappingContext, Options);
+    }
+}
+
+void UInputManagerComponent::RemoveMappingContexts(const TArray<UInputMappingContext*>& MappingContexts,
+    const FModifyContextOptions& Options)
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        for (UInputMappingContext* MappingContext : MappingContexts)
+        {
+            Subsystem->RemoveMappingContext(MappingContext, Options);
+        }
+    }
+}
+
+UEnhancedInputLocalPlayerSubsystem* UInputManagerComponent::GetEnhancedInputLocalPlayerSubsystem() const
+{
+    APlayerController* PlayerController = nullptr;
+
+    if (GetOwner()->IsA<APlayerController>())
+    {
+        PlayerController = Cast<APlayerController>(GetOwner());
+    }
+    else if (GetOwner()->IsA<APawn>())
+    {
+        PlayerController = Cast<APlayerController>(Cast<APawn>(GetOwner())->GetController());
+    }
+    else if (GetOwner()->IsA<AGameModeBase>() || GetOwner()->IsA<AGameStateBase>())
+    {
+        PlayerController = GetWorld()->GetFirstPlayerController();
+    }
+
+    return ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+}
---
+++ Source/InputManager/Public/Components/InputManagerComponent.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputSubsystemInterface.h"
+#include "Components/ActorComponent.h"
+#include "InputManagerComponent.generated.h"
+
+class UInputMappingContext;
+class UEnhancedInputLocalPlayerSubsystem;
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class INPUTMANAGER_API UInputManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TArray<TObjectPtr<UInputMappingContext>> DefaultMappingContexts;
+
+public:
+    UInputManagerComponent();
+
+    /* ActorComponent */
+
+    virtual void BeginPlay() override;
+    virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
+    /* ThisClass */
+
+    UFUNCTION(BlueprintCallable)
+    void AddMappingContext(const UInputMappingContext* MappingContext, int32 Priority = 0, const FModifyContextOptions& Options = FModifyContextOptions());
+
+    UFUNCTION(BlueprintCallable)
+    void AddMappingContexts(const TArray<UInputMappingContext*>& MappingContexts, int32 Priority = 0, const FModifyContextOptions& Options = FModifyContextOptions());
+
+    UFUNCTION(BlueprintCallable)
+    void RemoveMappingContext(const UInputMappingContext* MappingContext, const FModifyContextOptions& Options = FModifyContextOptions());
+
+    UFUNCTION(BlueprintCallable)
+    void RemoveMappingContexts(const TArray<UInputMappingContext*>& MappingContexts, const FModifyContextOptions& Options = FModifyContextOptions());
+
+protected:
+    /* ThisClass */
+
+    UFUNCTION(BlueprintPure)
+    UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputLocalPlayerSubsystem() const;
+};
