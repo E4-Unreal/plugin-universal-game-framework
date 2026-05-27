eac7ced1cb9e805f072c471cc875dfb1b45d7080
Eu4ng <eu4ng97@gmail.com>
2026-05-28 04:30:21 +0900

    feat: WidgetManagerComponent 클래스 추가

---
+++ Source/WidgetManager/Private/Components/WidgetManagerComponent.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/WidgetManagerComponent.h"
+
+#include "Subsystems/WidgetManagerSubsystem.h"
+
+
+UWidgetManagerComponent::UWidgetManagerComponent()
+{
+}
+
+void UWidgetManagerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    for (TSubclassOf<UUserWidget> DefaultWidgetClass : DefaultWidgetClasses)
+    {
+        ShowWidget(DefaultWidgetClass);
+    }
+}
+
+void UWidgetManagerComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+    for (TSubclassOf<UUserWidget> DefaultWidgetClass : DefaultWidgetClasses)
+    {
+        HideWidget(DefaultWidgetClass);
+    }
+
+    Super::OnComponentDestroyed(bDestroyingHierarchy);
+}
+
+void UWidgetManagerComponent::ShowWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    if (auto Subsystem = UWidgetManagerSubsystem::Get(this))
+    {
+        Subsystem->ShowWidget(WidgetClass);
+    }
+}
+
+bool UWidgetManagerComponent::HideWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    if (auto Subsystem = UWidgetManagerSubsystem::Get(this))
+    {
+        return Subsystem->HideWidget(WidgetClass);
+    }
+
+    return false;
+}
+
+void UWidgetManagerComponent::ToggleWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    if (auto Subsystem = UWidgetManagerSubsystem::Get(this))
+    {
+        Subsystem->ToggleWidget(WidgetClass);
+    }
+}
---
+++ Source/WidgetManager/Public/Components/WidgetManagerComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WidgetManagerComponent.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class WIDGETMANAGER_API UWidgetManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+    TSet<TSubclassOf<UUserWidget>> DefaultWidgetClasses;
+
+public:
+    UWidgetManagerComponent();
+
+    /* ActorComponent */
+
+    virtual void BeginPlay() override;
+    virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
+    /* ThisClass */
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ShowWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+    UFUNCTION(BlueprintCallable)
+    virtual bool HideWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ToggleWidget(TSubclassOf<UUserWidget> WidgetClass);
+};
