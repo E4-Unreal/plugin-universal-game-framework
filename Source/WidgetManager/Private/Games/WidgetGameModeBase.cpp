86fbfa3ed222e72ab0518f1adc218eca4132400a
Eu4ng <eu4ng97@gmail.com>
2026-05-28 18:36:26 +0900

    feat: WidgetGameModeBase 클래스 추가

---
+++ Source/WidgetManager/Private/Games/WidgetGameModeBase.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Games/WidgetGameModeBase.h"
+
+#include "Components/WidgetManagerComponent.h"
+
+const FName AWidgetGameModeBase::WidgetManagerName(TEXT("WidgetManager"));
+
+AWidgetGameModeBase::AWidgetGameModeBase(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    /* GameModeBase */
+
+    DefaultPawnClass = nullptr;
+    HUDClass = nullptr;
+
+    /* WidgetManager */
+
+    WidgetManager = CreateDefaultSubobject<UWidgetManagerComponent>(WidgetManagerName);
+}
---
+++ Source/WidgetManager/Public/Games/WidgetGameModeBase.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WidgetGameModeBase.generated.h"
+
+class UWidgetManagerComponent;
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class WIDGETMANAGER_API AWidgetGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+
+protected:
+    const static FName WidgetManagerName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Getter, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UWidgetManagerComponent> WidgetManager;
+
+public:
+    /* ThisClass */
+
+    AWidgetGameModeBase(const FObjectInitializer& ObjectInitializer);
+
+    /* Getter & Setter */
+
+    UWidgetManagerComponent* GetWidgetManager() const { return WidgetManager; }
+};
